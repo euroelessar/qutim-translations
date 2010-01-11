0008d402fcc79ba90750ca40b92551dc3cc6a1c6
MrFree <mrfree@mrfree-desktop.(none)>
2010-01-11 22:15:05 +1000

    RU dobavlen mrim.ts add mrim.ts

---
+++ ru_RU/mrim.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AccountWizardMain</name>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="14"/>
+        <source>WizardPage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="20"/>
+        <source>Email:</source>
+        <translation>Электропочта:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="33"/>
+        <source>@mail.ru</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="38"/>
+        <source>@inbox.ru</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="43"/>
+        <source>@bk.ru</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="48"/>
+        <source>@list.ru</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="53"/>
+        <source>@corp.mail.ru</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/wizards/accountwizardmain.ui" line="63"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="39"/>
+        <source>Peter Rusanov</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MrimConnection</name>
+    <message>
+        <location filename="../../src/base/mrimconnection.cpp" line="210"/>
+        <source>Authentication failed!</source>
+        <translation type="unfinished">Ошибка аутентификации!</translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimconnection.cpp" line="219"/>
+        <source>Another client with same login connected!</source>
+        <translation type="unfinished">Другой клиент подключился с тем же логином!</translation>
+    </message>
+</context>
+<context>
+    <name>MrimPacket</name>
+    <message>
+        <location filename="../../src/base/mrimpacket.cpp" line="175"/>
+        <source>No error</source>
+        <translatorcomment>!?</translatorcomment>
+        <translation type="unfinished">Без ошибок</translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimpacket.cpp" line="177"/>
+        <source>Cannot read from socket</source>
+        <translation type="unfinished">Ошибка чтения из сокета</translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimpacket.cpp" line="179"/>
+        <source>Header is corrupted</source>
+        <translation type="unfinished">Заголовок поврежден</translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimpacket.cpp" line="181"/>
+        <source>Unknown error</source>
+        <translation type="unfinished">Неизвестная ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="33"/>
+        <source>Module-based realization of Mail.Ru IM protocol</source>
+        <translation type="unfinished">Модуль реализации Mail.ru IM протокола (MRIM)</translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="34"/>
+        <source>MRIM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="47"/>
+        <source>MRIM account creator</source>
+        <translation type="unfinished">Мастер создания учетной записи MRIM </translation>
+    </message>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="48"/>
+        <source>Account creator for module-based realization of Mail.Ru IM protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Roster</name>
+    <message>
+        <location filename="../../src/base/roster.cpp" line="195"/>
+        <source>Phone contacts</source>
+        <translation type="unfinished">Телефонные контакты</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/base/mrimplugin.cpp" line="40"/>
+        <source>Author</source>
+        <translation type="unfinished">Автор</translation>
+    </message>
+</context>
+</TS>
