ac4d30b7f3547a9ac58fd60fb48c6065addb0e4f
MrFree <mrfree@mrfree-desktop.(none)>
2010-01-16 21:08:07 +1000

    RU add Qt, yandex.narod

---
+++ ru_RU/yanarod.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>YandexNarodAuthorizator</name>
+    <message>
+        <location filename="../../src/yandexnarodauthorizator.cpp" line="70"/>
+        <source>Has no login or password</source>
+        <translation type="unfinished">Не указан ник или пароль</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodauthorizator.cpp" line="79"/>
+        <source>Authorization succeed</source>
+        <translation type="unfinished">Авторизация выполнена</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodauthorizator.cpp" line="83"/>
+        <source>Authorization failured</source>
+        <translation type="unfinished">Не удалось авторизоваться</translation>
+    </message>
+</context>
+<context>
+    <name>YandexNarodManager</name>
+    <message>
+        <location filename="../../src/yandexnarodmanage.cpp" line="25"/>
+        <source>Yandex.Narod file manager</source>
+        <translation type="unfinished">Менеджер файлов Yandex.Narod</translation>
+    </message>
+</context>
+<context>
+    <name>YandexNarodNetMan</name>
+    <message>
+        <location filename="../../src/yandexnarodnetman.cpp" line="139"/>
+        <source>Authorizing...</source>
+        <translation type="unfinished">Авторизация...</translation>
+    </message>
+</context>
+<context>
+    <name>YandexNarodSettingsClass</name>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="14"/>
+        <location filename="../../src/yandexnarodsettings.ui" line="24"/>
+        <source>Settings</source>
+        <translation type="unfinished">Настройки</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="48"/>
+        <source>Password</source>
+        <translation type="unfinished">пароль</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="68"/>
+        <source>Login</source>
+        <translation type="unfinished">Логин</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="75"/>
+        <source>Test Authorization</source>
+        <translation type="unfinished">Проверить логин</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="82"/>
+        <source>status</source>
+        <translation type="unfinished">статус</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="105"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Verdana&apos;;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="116"/>
+        <source>Send file template</source>
+        <translation type="unfinished">Шаблон сообщения</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="129"/>
+        <source>%N - file name; %U - file URL; %S - file size</source>
+        <translation type="unfinished">%N -имя файла; %U - ссылка; %S - размер файла</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="139"/>
+        <source>About</source>
+        <translation type="unfinished">О плагине</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodsettings.ui" line="145"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Tahoma&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/yandexnarodlogo.png&quot; /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-weight:600;&quot;&gt;Yandex.Narod qutIM plugin&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;svn version&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;File exchange via &lt;/span&gt;&lt;a href=&quot;http://narod.yandex.ru&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#0057ae;&quot;&gt;Yandex.Narod&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt; service&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-weight:600;&quot;&gt;Author: &lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;Alexander Kazarin&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;mailto:boiler.co.ru&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#0000ff;&quot;&gt;boiler@co.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#000000;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#000000;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; color:#000000;&quot;&gt;(c) 2008-2009&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Tahoma&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/yandexnarodlogo.png&quot; /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-weight:600;&quot;&gt;Плагин Yandex.Narod для qutIM&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;git version&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;Обмен файлами черех &lt;/span&gt;&lt;a href=&quot;http://narod.yandex.ru&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#0057ae;&quot;&gt;Yandex.Narod&lt;/span&gt;&lt;/a&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-weight:600;&quot;&gt;Автор: &lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;;&quot;&gt;Александр Казарин&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;mailto:boiler.co.ru&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#0000ff;&quot;&gt;boiler@co.ru&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#000000;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;; text-decoration: underline; color:#000000;&quot;&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; color:#000000;&quot;&gt;(c) 2008-2010&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>YandexNarodUploadDialog</name>
+    <message>
+        <location filename="../../src/uploaddialog.cpp" line="116"/>
+        <source>Choose file for %1</source>
+        <translation type="unfinished">Выберите файл для %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.cpp" line="116"/>
+        <source>Choose file for uploading</source>
+        <translation type="unfinished">Выберите файл для загрузки</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.cpp" line="128"/>
+        <source>Authorizing...</source>
+        <translation type="unfinished">Авторизация...</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.cpp" line="164"/>
+        <source>Getting storage...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.cpp" line="187"/>
+        <source>Uploading...</source>
+        <translation type="unfinished">Загрузка на сервер...</translation>
+    </message>
+</context>
+<context>
+    <name>requestAuthDialogClass</name>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="26"/>
+        <source>Authorization</source>
+        <translation type="unfinished">Авторизация</translation>
+    </message>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="38"/>
+        <source>Login:</source>
+        <translation type="unfinished">Логин:</translation>
+    </message>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="55"/>
+        <source>Password:</source>
+        <translation type="unfinished">Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="75"/>
+        <source>Remember</source>
+        <translation type="unfinished">Запомнить</translation>
+    </message>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="96"/>
+        <source>Captcha:</source>
+        <translation type="unfinished">Капча:</translation>
+    </message>
+    <message>
+        <location filename="../../src/requestauthdialog.ui" line="104"/>
+        <source>about:blank</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>uploadDialogClass</name>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="19"/>
+        <source>Uploading...</source>
+        <translation type="unfinished">Загрузка на сервер...</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="29"/>
+        <source>Upload started.</source>
+        <translation type="unfinished">Выполняется загрузка.</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="39"/>
+        <source>File: </source>
+        <translation type="unfinished">файл:</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="49"/>
+        <source>Progress:</source>
+        <translation type="unfinished">Прогресс:</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="56"/>
+        <source>Elapsed time:</source>
+        <translation type="unfinished">Осталось времени:</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="63"/>
+        <source>Speed:</source>
+        <translation type="unfinished">Скорость:</translation>
+    </message>
+    <message>
+        <location filename="../../src/uploaddialog.ui" line="92"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Отменить</translation>
+    </message>
+</context>
+<context>
+    <name>yandexnarodManageClass</name>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="71"/>
+        <source>Get Filelist</source>
+        <translation type="unfinished">Получить файлов</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="82"/>
+        <source>Upload File</source>
+        <translation type="unfinished">Загрузить файл</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="126"/>
+        <source>Actions:</source>
+        <translation type="unfinished">Действия:</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="139"/>
+        <source>Clipboard</source>
+        <translation type="unfinished">Ссылку в буфер</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="159"/>
+        <source>Delete File</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="210"/>
+        <source>line1
+line2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="252"/>
+        <source>Close</source>
+        <translation type="unfinished">Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="287"/>
+        <source>Files list:</source>
+        <translation type="unfinished">Список файлов:</translation>
+    </message>
+    <message>
+        <location filename="../../src/yandexnarodmanage.ui" line="304"/>
+        <source>New Item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
