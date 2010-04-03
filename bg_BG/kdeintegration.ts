1e7ac25a892a76a3dd22f64034759be0495859c8
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-04-03 15:53:14 +0300

    BG: KDE Integration updates

--- bg_BG/kdeintegration.ts
+++ bg_BG/kdeintegration.ts
@@ -13,6 +13,19 @@
     </message>
 </context>
 <context>
+    <name>KDENotificationLayer</name>
+    <message>
+        <location filename="../../kde-integration/src/notification/kdenotificationlayer.cpp" line="+121"/>
+        <source>Open chat</source>
+        <translation>Отваряне на разговор</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Close</source>
+        <translation>Затваряне</translation>
+    </message>
+</context>
+<context>
     <name>KdeSpellerSettings</name>
     <message>
         <location filename="../../kde-integration/src/speller/kdespellersettings.ui" line="+14"/>
@@ -68,12 +81,71 @@
         <source>Spell checker</source>
         <translation>Проверка на правописа</translation>
     </message>
+    <message>
+        <location filename="../../kde-integration/src/notification/kdenotificationlayer.cpp" line="-83"/>
+        <source>Popups</source>
+        <translation>Изскачащи прозорци</translation>
+    </message>
 </context>
 <context>
     <name>Task</name>
     <message>
         <source>Developer</source>
         <translation type="obsolete">Разработчици</translation>
+    </message>
+</context>
+<context>
+    <name>plugmanSettings</name>
+    <message>
+        <location filename="../../kde-integration/src/notification/settings.ui" line="+13"/>
+        <source>Form</source>
+        <translation>Добавки</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Installed plugins:</source>
+        <translation>Инсталирани добавки</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Install from internet</source>
+        <translation>Инсталиране от Интернет</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Install from file</source>
+        <translation>Инсталиране от файл</translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>About</source>
+        <translation>За добавката</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/plugin-logo.png&quot; /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Simple qutIM plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Author: &lt;/span&gt;Sidorov Aleksey&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Contacts: &lt;/span&gt;&lt;a href=&quot;mailto::sauron@citadelspb.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;sauron@citadeslpb.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/plugin-logo.png&quot; /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Simple qutIM plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Автор: &lt;/span&gt;Алексей Сидоров&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Контакти &lt;/span&gt;&lt;a href=&quot;mailto::sauron@citadelspb.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;sauron@citadeslpb.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
