2eb4727f94e974d5e0b25314362b1865572c9774
MrFree <f1.mrfree@gmail.com>
2010-04-04 00:58:42 +1100

    RU добавлен kde-integration

---
+++ ru_RU/kdeintegration.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="42"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="45"/>
+        <source>Sidorov Aleksey</source>
+        <translation type="unfinished">Сидоров Алексей</translation>
+    </message>
+</context>
+<context>
+    <name>KDENotificationLayer</name>
+    <message>
+        <location filename="../../src/notification/kdenotificationlayer.cpp" line="121"/>
+        <source>Open chat</source>
+        <translation type="unfinished">Открыть чат</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/kdenotificationlayer.cpp" line="121"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+</context>
+<context>
+    <name>KdeSpellerSettings</name>
+    <message>
+        <location filename="../../src/speller/kdespellersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/speller/kdespellersettings.ui" line="29"/>
+        <source>Select dictionary:</source>
+        <translation type="unfinished">Словарь:</translation>
+    </message>
+    <message>
+        <location filename="../../src/speller/kdespellersettings.ui" line="39"/>
+        <source>Autodetect of language</source>
+        <translation type="unfinished">Автоматически определить язык</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="48"/>
+        <source>KDE integration</source>
+        <translation type="unfinished">Интеграция с KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="49"/>
+        <source>Integration with K Desktop Environment</source>
+        <translation type="unfinished">Модуль интеграции с графической средой KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="51"/>
+        <source>KDE Emoticons</source>
+        <translation type="unfinished">Смайлы KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="52"/>
+        <source>Using KDE emoticons packs</source>
+        <translation type="unfinished">Модуль для использования смайлов KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="54"/>
+        <source>KDE Crash handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="55"/>
+        <source>Handles qutIM&apos;s craches by KCrash</source>
+        <translation type="unfinished">Перехват падения qutim программой KCrash</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="57"/>
+        <source>KDE Spell checker</source>
+        <translation type="unfinished">Проверка орфографии KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="60"/>
+        <source>KDE Icon engine</source>
+        <translation type="unfinished">Двидок иконок KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="61"/>
+        <source>Uses KDE Icon engine for providing icons</source>
+        <translation type="unfinished">Использует движок KDE для иконок</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="63"/>
+        <source>KDE Notifications</source>
+        <translation type="unfinished">Уведомления KDE</translation>
+    </message>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="64"/>
+        <source>Uses KDE notifications</source>
+        <translation type="unfinished">Модуль уведомлений KDE, так то</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../src/speller/kdespellerlayer.cpp" line="29"/>
+        <source>Spell checker</source>
+        <translation type="unfinished">Проверялка орфограции</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/kdenotificationlayer.cpp" line="38"/>
+        <source>Popups</source>
+        <translation type="unfinished">Уведомления</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/kdeintegration.cpp" line="43"/>
+        <location filename="../../src/kdeintegration.cpp" line="46"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+<context>
+    <name>plugmanSettings</name>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="13"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="23"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="29"/>
+        <source>Installed plugins:</source>
+        <translation type="unfinished">Установленые плагинов:</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="49"/>
+        <source>Install from internet</source>
+        <translation type="unfinished">Установить из сети</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="60"/>
+        <source>Install from file</source>
+        <translation type="unfinished">Установить из файла</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="92"/>
+        <source>About</source>
+        <translation>О плагине</translation>
+    </message>
+    <message>
+        <location filename="../../src/notification/settings.ui" line="104"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/plugin-logo.png&quot; /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Simple qutIM plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Author: &lt;/span&gt;Sidorov Aleksey&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Contacts: &lt;/span&gt;&lt;a href=&quot;mailto::sauron@citadelspb.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;sauron@citadeslpb.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/icons/plugin-logo.png&quot; /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Simple qutIM plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Автор: &lt;/span&gt;Сидоров Алексей&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Электропочта: &lt;/span&gt;&lt;a href=&quot;mailto::sauron@citadelspb.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;sauron@citadeslpb.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
