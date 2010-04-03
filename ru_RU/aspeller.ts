49690899235dd07b75c2fb252a18e68c5861d3fe
MrFree <f1.mrfree@gmail.com>
2010-04-04 00:50:15 +1100

    RU Добавлен aspell

---
+++ ru_RU/aspeller.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/aspellplugin.cpp" line="26"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation type="unfinished">Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/aspellplugin.cpp" line="29"/>
+        <location filename="../../src/aspellplugin.cpp" line="32"/>
+        <source>ASpell checker</source>
+        <translation type="unfinished">Проверка орфографии</translation>
+    </message>
+    <message>
+        <location filename="../../src/aspellplugin.cpp" line="30"/>
+        <location filename="../../src/aspellplugin.cpp" line="33"/>
+        <source>Check spelling by ASpell library</source>
+        <translation type="unfinished">Модуль проверки орфограции основанный на ASpell</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/aspellplugin.cpp" line="27"/>
+        <source>Developer</source>
+        <translation type="unfinished">Разработчик</translation>
+    </message>
+</context>
+</TS>
