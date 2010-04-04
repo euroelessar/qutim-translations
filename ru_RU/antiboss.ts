fbfe0bc73163996677b7860469fa9c652376c9f5
MrFree <f1.mrfree@gmail.com>
2010-04-04 11:24:51 +1100

    RU добавлен антибосс тут пришлось сделать про файл и с него генерить, цмак ничего не выдал...

---
+++ ru_RU/antiboss.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AntiBoss</name>
+    <message>
+        <location filename="antibossplugin.cpp" line="+36"/>
+        <source>Show/hide all windows</source>
+        <translation type="unfinished">Показать/скрыть все окна</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Antiboss</source>
+        <translation type="unfinished">АнтиБосс</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location line="-9"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+3"/>
+        <source>Anti-boss plugin</source>
+        <translation type="unfinished">Плагин АнтиБосс</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Plugin makes your chatting invisible for boss</source>
+        <translation type="unfinished">Плагин делающий невидимым кутим для вашего босса</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-3"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+</TS>
