bdd10ee4966b25125e5825ce7ded00ec13660311
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-04-10 00:34:46 +0300

    BG: MassMessaging updates

--- bg_BG/massmessaging.ts
+++ bg_BG/massmessaging.ts
@@ -4,38 +4,31 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="14"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="228"/>
+        <location filename="../../plugins/massmessaging/src/messagingdialog.ui" line="+14"/>
         <source>Multiply Sending</source>
         <translation>Многократно изпращане</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="136"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="232"/>
         <source>Items</source>
-        <translation>Списък</translation>
+        <translation type="obsolete">Списък</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="160"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="233"/>
+        <location line="+60"/>
         <source>Message</source>
         <translation>Съобщение</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="190"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="234"/>
+        <location line="+30"/>
         <source>15</source>
         <translation>15</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="206"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="235"/>
+        <location line="+16"/>
         <source>Interval (in seconds):</source>
         <translation>Интервал (в сек.):</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="228"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="236"/>
+        <location line="+22"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -58,30 +51,28 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:1; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="256"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="247"/>
+        <location line="+28"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;TextLabel&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;In progress...&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;В ход...&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="296"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="253"/>
         <source>Actions</source>
-        <translation>Действия</translation>
+        <translation type="obsolete">Действия</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="33"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="229"/>
         <source>Stop</source>
-        <translation>Спиране</translation>
+        <translation type="obsolete">Спиране</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui/messagingdialog.ui" line="46"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/ui_messagingdialog.h" line="230"/>
+        <location line="+59"/>
         <source>Send</source>
         <translation>Изпращане</translation>
     </message>
@@ -89,76 +80,98 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Manager</name>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/manager.cpp" line="19"/>
         <source>Accounts</source>
-        <translation>Сметки</translation>
+        <translation type="obsolete">Сметки</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/manager.cpp" line="29"/>
         <source>Unknown</source>
-        <translation>Неизвестен</translation>
+        <translation type="obsolete">Неизвестен</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/manager.cpp" line="87"/>
         <source>Error: message is empty</source>
-        <translation>Грешка: съобщението е празно</translation>
+        <translation type="obsolete">Грешка: съобщението е празно</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/manager.cpp" line="148"/>
         <source>Error: unknown account : %1</source>
-        <translation>Грешка: неизвестна сметка : %1</translation>
+        <translation type="obsolete">Грешка: неизвестна сметка : %1</translation>
+    </message>
+</context>
+<context>
+    <name>MassMessaging</name>
+    <message>
+        <location filename="../../plugins/massmessaging/src/messaging.cpp" line="+13"/>
+        <source>&amp;Mass Messaging</source>
+        <translation>&amp;Многократно изпращане</translation>
+    </message>
+</context>
+<context>
+    <name>MassMessaging::MessagingDialog</name>
+    <message>
+        <location filename="../../plugins/massmessaging/src/messagingdialog.cpp" line="+54"/>
+        <source>Stop</source>
+        <translation>Спиране</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Sending message to %1: %v/%m</source>
+        <translation>Изпращане на съобщение до %1: %v/%m</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Sending message to %1</source>
+        <translation>Изпращане на съобщение до %1</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Sending message to %1 (%2/%3), time remains: %4</source>
+        <translation>Изпращане на съобщение до %1 (%2/%3), оставащо време: %4</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Start</source>
+        <translation>Начало</translation>
     </message>
 </context>
 <context>
     <name>Messaging</name>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messaging.cpp" line="38"/>
         <source>Multiply Sending</source>
-        <translation>Многократно изпращане</translation>
+        <translation type="obsolete">Многократно изпращане</translation>
     </message>
 </context>
 <context>
     <name>MessagingDialog</name>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="47"/>
         <source>Actions</source>
-        <translation>Действия</translation>
+        <translation type="obsolete">Действия</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="49"/>
         <source>Load buddy list</source>
-        <translation>Зареждане на списък с контакти</translation>
+        <translation type="obsolete">Зареждане на списък с контакти</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="54"/>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="104"/>
         <source>Save buddy list</source>
-        <translation>Съхраняване на списъка с контакти</translation>
+        <translation type="obsolete">Съхраняване на списъка с контакти</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="86"/>
         <source>Multiply sending: all jobs finished</source>
-        <translation>Многократно изпращане: всички задачи са приключени</translation>
+        <translation type="obsolete">Многократно изпращане: всички задачи са приключени</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="93"/>
         <source>Load custom buddy list</source>
-        <translation>Зареждане на личен списък с контакти</translation>
+        <translation type="obsolete">Зареждане на личен списък с контакти</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="128"/>
         <source>Sending message to %1: %v/%m</source>
-        <translation>Изпращане на съобщение до %1: %v/%m</translation>
+        <translation type="obsolete">Изпращане на съобщение до %1: %v/%m</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="129"/>
         <source>Sending message to %1</source>
-        <translation>Изпращане на съобщение до %1</translation>
+        <translation type="obsolete">Изпращане на съобщение до %1</translation>
     </message>
     <message>
-        <location filename="../../../qutim/trunk/plugins/MassMessaging/src/messagingdialog.cpp" line="133"/>
         <source>Sending message to %1 (%2/%3), time remains: %4</source>
-        <translation>Изпращане на съобщение до %1 (%2/%3), оставащо време: %4</translation>
+        <translation type="obsolete">Изпращане на съобщение до %1 (%2/%3), оставащо време: %4</translation>
     </message>
 </context>
 </TS>
