6c190bc9c5f3c6568c8e59a2a8109cd564b36fc5
MrFree <f1.mrfree@gmail.com>
2010-04-13 21:58:40 +1100

    RU добавил спамилку и хистмэн

---
+++ ru_RU/histman.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/historymanagerplugin.cpp" line="+25"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>ChooseClientPage</name>
+    <message>
+        <location filename="../../src/chooseclientpage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation>Мастер</translation>
+    </message>
+</context>
+<context>
+    <name>ChooseOrDumpPage</name>
+    <message>
+        <location filename="../../src/chooseordumppage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation>Мастер</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Import history from one more client</source>
+        <translation>Импортировать историю из следующего клиента</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Dump history</source>
+        <translation>Сохранить историю</translation>
+    </message>
+</context>
+<context>
+    <name>ClientConfigPage</name>
+    <message>
+        <location filename="../../src/clientconfigpage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation>Мастер</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Path to profile:</source>
+        <translation>Путь к профилю:</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Encoding:</source>
+        <translation>Кодировка:</translation>
+    </message>
+    <message>
+        <location filename="../../src/clients/gajim.cpp" line="+99"/>
+        <location filename="../../src/clients/psi.cpp" line="+162"/>
+        <source>Select your Jabber account.</source>
+        <translation>Выберите ваш Jabber аккаунт.</translation>
+    </message>
+    <message>
+        <location filename="../../src/clients/qipinfium.cpp" line="+180"/>
+        <source>Select accounts for each protocol in the list.</source>
+        <translation>Выберите аккаунты для каждого протокола в списке.</translation>
+    </message>
+</context>
+<context>
+    <name>DumpHistoryPage</name>
+    <message>
+        <location filename="../../src/dumphistorypage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation>Мастер</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Choose format:</source>
+        <translation>Выберите формат:</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>JSON</source>
+        <translation>JSON (используется по умолчанию)</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Binary</source>
+        <translation>Бинарный</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Merging history state:</source>
+        <translation>Обьединение истории:</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Dumping history state:</source>
+        <translation>Сохранение истории:</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::ChooseClientPage</name>
+    <message>
+        <location filename="../../src/chooseclientpage.cpp" line="+45"/>
+        <source>Client</source>
+        <translation>Клиент</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Choose client which history you want to import to qutIM.</source>
+        <translation>Выберите клиент, историю которого нужно импортировать в qutIM.</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::ChooseOrDumpPage</name>
+    <message>
+        <location filename="../../src/chooseordumppage.cpp" line="+27"/>
+        <source>What to do next?</source>
+        <comment>Dump history or choose next client</comment>
+        <translation>Что дальше?</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>It is possible to choose another client for import history or dump history to the disk.</source>
+        <translation>Можно выбрать другой клиент для импортирования или сохранения истории на диск.</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::ClientConfigPage</name>
+    <message>
+        <location filename="../../src/clientconfigpage.cpp" line="+45"/>
+        <source>Configuration</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Enter path of your %1 profile file.</source>
+        <translation>Укажите путь к файлу профиля %1.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Enter path of your %1 profile dir.</source>
+        <translation>Укажите путь к папке профиля %1.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>If your history encoding differs from the system one, choose the appropriate encoding for history.</source>
+        <translation>Если кодировка истории отличается от ;системной, выберите требуемую.</translation>
+    </message>
+    <message>
+        <location line="+89"/>
+        <location line="+2"/>
+        <source>Select path</source>
+        <translation>Укажите путь</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::DumpHistoryPage</name>
+    <message>
+        <location filename="../../src/dumphistorypage.cpp" line="+58"/>
+        <source>Dumping</source>
+        <translation>Сожранение</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Last step. Click &apos;Dump&apos; to start dumping process.</source>
+        <translation>Последний шаг. Нажмите &quot;Сохранить&quot; для обьединения историй.</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Manager merges history, it make take several minutes.</source>
+        <translation>Обьединение истории, это займет некоторое время.</translation>
+    </message>
+    <message>
+        <location line="+27"/>
+        <source>History has been succesfully imported.</source>
+        <translation>История успешно импортирована.</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::HistoryManagerWindow</name>
+    <message>
+        <location filename="../../src/historymanagerwindow.cpp" line="+75"/>
+        <location line="+202"/>
+        <source>History manager</source>
+        <translation>Менеджер истории</translation>
+    </message>
+    <message>
+        <location line="-192"/>
+        <source>&amp;Dump</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManager::ImportHistoryPage</name>
+    <message>
+        <location filename="../../src/importhistorypage.cpp" line="+44"/>
+        <source>Loading</source>
+        <translation>Загрузка</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Manager loads all history to memory, it may take several minutes.</source>
+        <translation>Менеджер загружает всю историю в память, это может занять несколько минут.</translation>
+    </message>
+    <message numerus="yes">
+        <location line="+26"/>
+        <source>%n message(s) have been succesfully loaded to memory.</source>
+        <translation>
+            <numerusform>%n сообщение было полностью заружено в память.</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+1"/>
+        <source>It has taken %n ms.</source>
+        <translation>
+            <numerusform>Это потребует %n минуту.</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>HistoryManagerWindow</name>
+    <message>
+        <location filename="../../src/historymanagerwindow.ui" line="+16"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImportHistoryPage</name>
+    <message>
+        <location filename="../../src/importhistorypage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation>Мастер</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/historymanagerplugin.cpp" line="+3"/>
+        <source>HistMan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Plugin makes possible to import history to qutIM from different messengers</source>
+        <translation type="unfinished">Плагин дающий возможность импортировать историю в qutIM из прочих IM</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Import history</source>
+        <translation>Импорт истории</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-12"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
