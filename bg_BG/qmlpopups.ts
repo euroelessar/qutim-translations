47f6d68cef4c9c4bb8c6dd24503a075f779ea822
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-05-02 13:28:18 +0300

    BG: QML popups updates

--- bg_BG/qmlpopups.ts
+++ bg_BG/qmlpopups.ts
@@ -2,37 +2,222 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="bg_BG">
 <context>
+    <name>AppearanceSettings</name>
+    <message>
+        <location filename="../../plugins/qmlpopups/src/settings/popupappearance.ui" line="+20"/>
+        <source>Form</source>
+        <translation>Настройки на изгледа</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>test</source>
+        <translation>Проба</translation>
+    </message>
+</context>
+<context>
+    <name>BehaviorSettings</name>
+    <message>
+        <location filename="../../plugins/qmlpopups/src/settings/notifications.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки на изгледа</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Notification types</source>
+        <translation>Тип на уведомленията</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Timeout, msec:</source>
+        <translation>Интервал (мсек):</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>5000</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Animation speed:</source>
+        <translation>Скорост на анимацията:</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>No Animation</source>
+        <translation>Без анимация</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Very fast</source>
+        <translation>Много бързо</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Fast</source>
+        <translation>Бързо</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Normal</source>
+        <translation>Нормално</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Slow</source>
+        <translation>Бавно</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Very slow</source>
+        <translation>Много бавно</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Custom</source>
+        <translation>Собсвена</translation>
+    </message>
+</context>
+<context>
+    <name>Notifications</name>
+    <message>
+        <location filename="../../plugins/qmlpopups/src/settings/popupbehavior.cpp" line="+28"/>
+        <source>User online</source>
+        <translation>Потребител идва на линия</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>User offline</source>
+        <translation>Потребител излиза извън линия</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Status changed</source>
+        <translation>Промяна на статуса</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>User has birthday today!</source>
+        <translation>Потребител има рожден ден днес!</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>qutIM Launched</source>
+        <translation>qutIM е стартиран</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Message recieved</source>
+        <translation>Получено съобщение</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Message sent</source>
+        <translation>Изпратено съобщение</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>System notification</source>
+        <translation>Системно уведомление</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Typing</source>
+        <translation>Пише</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Message blocked</source>
+        <translation>Блокирано съобщение</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Count</source>
+        <translation>Брой</translation>
+    </message>
+</context>
+<context>
     <name>Plugin</name>
     <message>
-        <location filename="../../plugins/qmlpopups/src/qmlpopups.cpp" line="+11"/>
+        <location filename="../../plugins/qmlpopups/src/qmlpopups.cpp" line="+15"/>
         <source>Qml popups</source>
         <translation>Qml изскачащи прозорци</translation>
     </message>
     <message>
         <location line="+1"/>
+        <source>Modern popups, based on power of Qt Quick</source>
+        <translation>Модерни изскачащи прозорци, на базата на Qt Quick</translation>
+    </message>
+    <message>
         <source>Nice popups ^_^ TODO</source>
-        <translation>Хубави прозорци ^_^ TODO</translation>
+        <translation type="obsolete">Хубави прозорци ^_^ TODO</translation>
     </message>
 </context>
 <context>
     <name>QmlPopups::Popup</name>
     <message>
-        <location filename="../../plugins/qmlpopups/src/popup.cpp" line="+64"/>
+        <location filename="../../plugins/qmlpopups/src/popup.cpp" line="+74"/>
         <source>&lt;p&gt; + %1 more notifications &lt;/p&gt;</source>
         <translation>&lt;p&gt; + още %1 уведомления &lt;/p&gt;</translation>
     </message>
 </context>
 <context>
+    <name>QmlPopups::PopupAppearance</name>
+    <message>
+        <location filename="../../plugins/qmlpopups/src/settings/popupappearance.cpp" line="+33"/>
+        <location line="+64"/>
+        <location line="+9"/>
+        <source>Preview</source>
+        <translation>Предварителен преглед</translation>
+    </message>
+    <message>
+        <location line="-9"/>
+        <source>This is a simple popup</source>
+        <translation>Това е опростен изскачащ прозорец</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple message</source>
+        <translation>Просто съобщение</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Another message</source>
+        <translation>Друго съобщение</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Simple messagebox</source>
+        <translation>Проста област със съобщения</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../plugins/qmlpopups/src/qmlpopups.cpp" line="+3"/>
+        <source>Notifications</source>
+        <translation>Уведомления</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Popups</source>
+        <translation>Изскачащи прозорци</translation>
+    </message>
+</context>
+<context>
     <name>plugin</name>
     <message>
-        <location filename="../../plugins/qmlpopups/src/qmlpopups.cpp" line="+2"/>
+        <location line="-3"/>
         <source>Qml popups</source>
         <translation>Qml изскачащи прозорци</translation>
     </message>
     <message>
         <location line="+0"/>
+        <source>Modern popups, based on power of Qt Quick</source>
+        <translation>Модерни изскачащи прозорци, на базата на Qt Quick</translation>
+    </message>
+    <message>
         <source>description</source>
-        <translation>Описание</translation>
+        <translation type="obsolete">Описание</translation>
     </message>
 </context>
 </TS>
