b9b4a070dd8298ddc076342e9d2369fd4431968a
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-05-27 11:50:54 +0300

    BG: Few changes after (not fully tested) small change to QutimPlugin.cmake

---
+++ bg_BG/oscaridentify.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../protocols/oscar/plugins/identify/clientidentify.cpp" line="+38"/>
+        <source>Alexey Ignatiev</source>
+        <translation>Алексей Игнатиев</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Alexey Prokhin</source>
+        <translation>Елексей Прокхин</translation>
+    </message>
+</context>
+<context>
+    <name>ContactList</name>
+    <message>
+        <location line="+133"/>
+        <source>Possible client</source>
+        <translation>Вероятен клиент</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="-139"/>
+        <source>OscarIdentify</source>
+        <translation>Oscar идентификация</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location line="+9"/>
+        <source>Client identificaton for oscar contacts</source>
+        <translation>Идентификация на клиентите на Oscar контактите</translation>
+    </message>
+    <message>
+        <location line="-1"/>
+        <source>ICQ</source>
+        <translation>ICQ</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-5"/>
+        <location line="+3"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
---
+++ bg_BG/oscarxstatus.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../protocols/oscar/plugins/xstatus/xstatus.cpp" line="+155"/>
+        <source>Alexey Prokhin</source>
+        <translation>Елексей Прокхин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+4"/>
+        <source>ICQ</source>
+        <translation>ICQ</translation>
+    </message>
+    <message>
+        <location line="-7"/>
+        <source>OscarXStatus</source>
+        <translation>Oscar разширен статус</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location line="+7"/>
+        <source>Additional statuses for oscar protocol</source>
+        <translation>Допълнителни статуси към Oscar протокола</translation>
+    </message>
+</context>
+<context>
+    <name>Status</name>
+    <message>
+        <location line="+15"/>
+        <source>Angry</source>
+        <translation>Бесен</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Depression</source>
+        <translation>Депресиран</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>At home</source>
+        <translation>Вкъщи</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>At work</source>
+        <translation>На работа</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Eating</source>
+        <translation>Хапващ</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-27"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+<context>
+    <name>XStatus</name>
+    <message>
+        <location line="-122"/>
+        <source>Angry</source>
+        <translation>Бесен</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Taking a bath</source>
+        <translation>В банята</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Tired</source>
+        <translation>Уморен</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Party</source>
+        <translation>Купонясващ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Drinking beer</source>
+        <translation>Жулещ бира</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Thinking</source>
+        <translation>Размишляващ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Eating</source>
+        <translation>Хапващ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Watching TV</source>
+        <translation>Гледащ телевизия</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Meeting</source>
+        <translation>На среща</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Coffee</source>
+        <translation>На кафе</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Listening to music</source>
+        <translation>Слушащ музика</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Business</source>
+        <translation>По работа</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Shooting</source>
+        <translation>Снимащ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Having fun</source>
+        <translation>Забавляващ се</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>On the phone</source>
+        <translation>На телефона</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Gaming</source>
+        <translation>Играещ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Studying</source>
+        <translation>Учащ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Shopping</source>
+        <translation>Пазаруващ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Feeling sick</source>
+        <translation>Болен</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Sleeping</source>
+        <translation>Спящ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Surfing</source>
+        <translation>Сърфиращ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Browsing</source>
+        <translation>Ровещ се в Нета</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Working</source>
+        <translation>Бачкащ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Typing</source>
+        <translation>Набиращ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Picnic</source>
+        <translation>На тиферич</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Cooking</source>
+        <translation>Готвещ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Smoking</source>
+        <translation>Пушещ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>I&apos;m high</source>
+        <translation>Надрусан</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>On WC</source>
+        <translation>Друсащ крушата</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>To be or not to be</source>
+        <translation>Да бъда или да не бъда</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Watching pro7 on TV</source>
+        <translation>Гледащ телевизия PRO7</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Love</source>
+        <translation>Влюбен</translation>
+    </message>
+    <message>
+        <location line="+172"/>
+        <source>X-Status</source>
+        <translation>Разширен статус</translation>
+    </message>
+</context>
+</TS>
