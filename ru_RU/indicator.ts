b6ba372e42a50fa0df3ba48ab2ad523ffda3cd57
MrFree <f1.mrfree@gmail.com>
2010-06-02 20:10:07 +1100

    RU add indicator (ubuntu/gnome integration)

---
+++ ru_RU/indicator.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/indicator.cpp" line="+26"/>
+        <source>Vsevolod Velichko</source>
+        <translation>Всеволод Величко
+</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+4"/>
+        <location line="+5"/>
+        <source>Indicator</source>
+        <translation type="unfinished">Индиктор</translation>
+    </message>
+    <message>
+        <location line="-4"/>
+        <location line="+5"/>
+        <source>Ubuntu Indicator applet integration</source>
+        <translation type="unfinished">Интеграция в апплет панели гнома Убунту</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-9"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+</TS>
