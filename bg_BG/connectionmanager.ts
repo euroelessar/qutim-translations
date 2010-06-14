8836644c8f72d77114f9b1a570aa20283f3cf9c8
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-06-14 10:22:59 +0300

    BG: ConnectionManager updates

--- bg_BG/connectionmanager.ts
+++ bg_BG/connectionmanager.ts
@@ -4,15 +4,36 @@
 <context>
     <name>Author</name>
     <message>
-        <location filename="../../plugins/connectionmanager/src/connectionmanager.cpp" line="+42"/>
+        <location filename="../../plugins/connectionmanager/src/connectionmanager.cpp" line="+45"/>
         <source>Aleksey Sidorov</source>
         <translation>Алексей Сидоров</translation>
     </message>
 </context>
 <context>
+    <name>ManagerSettings</name>
+    <message>
+        <location filename="../../plugins/connectionmanager/src/managersettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Autoconnect on startup</source>
+        <translation>Автоматично свързване при стартиране</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../plugins/connectionmanager/src/connectionmanager.cpp" line="+16"/>
+        <source>Connection manager</source>
+        <translation>Управление на връзката</translation>
+    </message>
+</context>
+<context>
     <name>Task</name>
     <message>
-        <location line="+1"/>
+        <location line="-15"/>
         <source>Author</source>
         <translation>Автор</translation>
     </message>
