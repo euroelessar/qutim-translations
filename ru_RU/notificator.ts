41f14308a29c54dffea6fbae5d167ed24af737ce
MrFree <f1.mrfree@gmail.com>
2010-06-17 22:42:10 +1100

    RU add test notificator

---
+++ ru_RU/notificator.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/libnotificator.cpp" line="26"/>
+        <source>Vsevolod Velichko</source>
+        <translation>Всеволод Величко</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/libnotificator.cpp" line="30"/>
+        <location filename="../../src/libnotificator.cpp" line="35"/>
+        <source>Libnotificator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/libnotificator.cpp" line="31"/>
+        <location filename="../../src/libnotificator.cpp" line="36"/>
+        <source>Ubuntu libnotify popups provider</source>
+        <translation type="unfinished">Уведомления через прозрачные убунту уведомления</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/libnotificator.cpp" line="27"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+</TS>
