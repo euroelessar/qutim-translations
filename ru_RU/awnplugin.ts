0e30b7536b125fe04d6a1f1139539fd92ed6251c
MrFree <f1.mrfree@gmail.com>
2010-06-17 22:31:53 +1100

    RU add awn

---
+++ ru_RU/awnplugin.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AWNService</name>
+    <message>
+        <location filename="../../src/awnservice.cpp" line="274"/>
+        <source>Show/Hide</source>
+        <translation type="unfinished">Показать/Скрыть</translation>
+    </message>
+    <message>
+        <location filename="../../src/awnservice.cpp" line="275"/>
+        <source>Read all messages</source>
+        <translation type="unfinished">Прочесть все сообщения</translation>
+    </message>
+    <message>
+        <location filename="../../src/awnservice.cpp" line="276"/>
+        <source>Go online</source>
+        <translation type="unfinished">Подключиться</translation>
+    </message>
+    <message>
+        <location filename="../../src/awnservice.cpp" line="277"/>
+        <source>Go offline</source>
+        <translation type="unfinished">Отключиться</translation>
+    </message>
+    <message>
+        <location filename="../../src/awnservice.cpp" line="278"/>
+        <source>Exit QutIM</source>
+        <translation type="unfinished">Закрыть qutIM</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/awn.cpp" line="27"/>
+        <source>Stanislav (proDOOMman) Kosolapov</source>
+        <translation type="unfinished">Станислав (proDOOMman)  Косолапов</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/awn.cpp" line="31"/>
+        <location filename="../../src/awn.cpp" line="36"/>
+        <source>AWN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/awn.cpp" line="32"/>
+        <location filename="../../src/awn.cpp" line="37"/>
+        <source>Avant window navigator dock integration</source>
+        <translation type="unfinished">Интеграция в панель Авант дока</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/awn.cpp" line="28"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+</TS>
