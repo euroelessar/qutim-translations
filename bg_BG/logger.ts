7ec3982ca14a316c894045b54cb4a2a9bb7ed6ad
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-07-15 10:39:59 +0300

    BG: Added logger

---
+++ bg_BG/logger.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../plugins/logger/src/logger.cpp" line="+36"/>
+        <source>Aleksey Sidorov</source>
+        <translation>Алексей Сидоров</translation>
+    </message>
+</context>
+<context>
+    <name>Logger::LoggerPlugin</name>
+    <message>
+        <location line="+12"/>
+        <source>New session started, happy debuging ^_^</source>
+        <translation>Стартирана е нова сесия, успешно дебъгване ^_^</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="-15"/>
+        <source>Logger</source>
+        <translation>Журнал</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple file based logger for debug</source>
+        <translation>Опростен файлово-базиран журнал за дебъгване</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="+3"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
