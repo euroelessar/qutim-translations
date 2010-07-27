96feb4ecb1f65538b6522716463fe9b14c79ce68
MrFree <f1.mrfree@gmail.com>
2010-07-28 08:09:47 +1100

    RU phonon

---
+++ ru_RU/phononsound.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../plugins/phononsound/src/phononsoundplugin.cpp" line="+22"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+3"/>
+        <location line="+5"/>
+        <source>Phonon sound engine</source>
+        <translation type="unfinished">Звуковой движок Phonon</translation>
+    </message>
+    <message>
+        <location line="-4"/>
+        <location line="+5"/>
+        <source>Sound engine based on KDE sound engine</source>
+        <translation type="unfinished">Вывод звука основанный на звуковом движке KDE</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-8"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
