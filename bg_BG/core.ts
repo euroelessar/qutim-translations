83443edf3d08b52544ad65f7bce437dc4d8c6b25
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-08-29 15:10:17 +0300

    BG: Language style fixes

--- bg_BG/core.ts
+++ bg_BG/core.ts
@@ -403,7 +403,7 @@ p, li { white-space: pre-wrap; }
         <translation>about:blank</translation>
     </message>
     <message>
-        <location line="+52"/>
+        <location line="+36"/>
         <source>Chats</source>
         <translation>Разговори</translation>
     </message>
@@ -1194,7 +1194,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Core::XSettingsWindow</name>
     <message>
-        <location filename="../../qutim-meta/core/src/corelayers/xsettingsdialog/xsettingswindow.cpp" line="+230"/>
+        <location filename="../../qutim-meta/core/src/corelayers/xsettingsdialog/xsettingswindow.cpp" line="+237"/>
         <source>qutIM settings - %1</source>
         <translation>Настройки на qutIM - %1</translation>
     </message>
@@ -2313,7 +2313,7 @@ p, li { white-space: pre-wrap; }
         <translation>Изскачащи прозорци</translation>
     </message>
     <message>
-        <location filename="../../qutim-meta/core/src/corelayers/accountcreator/accountcreator.cpp" line="+8"/>
+        <location filename="../../qutim-meta/core/src/corelayers/accountcreator/accountcreator.cpp" line="+12"/>
         <source>Accounts</source>
         <translation>Сметки</translation>
     </message>
@@ -2391,7 +2391,7 @@ p, li { white-space: pre-wrap; }
         <translation>Разговор</translation>
     </message>
     <message>
-        <location filename="../../qutim-meta/core/src/corelayers/xsettingsdialog/xsettingswindow.cpp" line="-196"/>
+        <location filename="../../qutim-meta/core/src/corelayers/xsettingsdialog/xsettingswindow.cpp" line="-203"/>
         <source>General</source>
         <translation>Общи</translation>
     </message>
@@ -2495,7 +2495,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location line="+39"/>
         <source>Show extended icons</source>
-        <translation>Показване на разширени икони</translation>
+        <translation>Показване на икони за разширен статус</translation>
     </message>
     <message>
         <location line="-14"/>
