71974e73e830e96d17c9c31afc501c2c9e0e5fc1
MrFree <f1.mrfree@gmail.com>
2010-09-05 19:25:05 +1100

    RU add scriptapi && webhistory. Rename yanarod -> yandexnarod

---
+++ ru_RU/scriptapi.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/scriptplugin.cpp" line="+28"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+3"/>
+        <source>Script API</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Added ability to create plugins at JavaScript</source>
+        <translation type="unfinished">Модуль позволяет писать расширения на JavaScript</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-3"/>
+        <source>Developer</source>
+        <translation>Разработчик</translation>
+    </message>
+</context>
+</TS>
