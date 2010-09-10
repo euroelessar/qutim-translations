d5d4b8dd260e5e99460dbf80f22762437ed8c45d
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2010-09-10 12:32:22 +0300

    BG: Added DBus notifications

---
+++ bg_BG/dbusnotifications.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/dbusnotifications/src/dplugin.cpp" line="+18"/>
+        <source>Aleksey Sidorov</source>
+        <translation>Алексей Сидоров</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="-4"/>
+        <source>DBus notifications</source>
+        <translation>DBus уведомления</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system based on Freedesktop DBus protocol</source>
+        <translation>Система за уведомления на базата на Freedesktop DBus протокола</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="+4"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+<context>
+    <name>plugin</name>
+    <message>
+        <location line="+3"/>
+        <source>DBus notifications</source>
+        <translation>DBus уведомления</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system based on Freedesktop DBus protocol</source>
+        <translation>Система за уведомления на базата на Freedesktop DBus протокола</translation>
+    </message>
+</context>
+</TS>
