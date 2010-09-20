fc08f523f0b2453839f1854270a1a8c3ab04f41c
MrFree <f1.mrfree@gmail.com>
2010-09-20 22:21:14 +1100

    ru small upd

--- ru_RU/nowplaying.ts
+++ ru_RU/nowplaying.ts
@@ -4,20 +4,148 @@
 <context>
     <name>Author</name>
     <message>
-        <location filename="../../plugins/nowplaying/src/nowplaying.cpp" line="+32"/>
+        <location filename="../../src/nowplaying.cpp" line="+32"/>
         <source>Kazlauskas Ian</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Alexey Prokhin</source>
+        <translation>Алексей Прохин</translation>
+    </message>
+</context>
+<context>
+    <name>IcqSettings</name>
+    <message>
+        <location filename="../../src/icqsettings.ui" line="+14"/>
+        <source>ICQ</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Current mode</source>
+        <translation type="unfinished">Текущий режим</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Deactivated</source>
+        <translation type="unfinished">Отключен</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Changes current X-status message</source>
+        <translation type="unfinished">Изменять сообщение текущего Х-статуса</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location line="+33"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%artist - artist&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%title - title&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%album - album&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%track - track number&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%time - track length&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%file - file name&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%uri - full path to file&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%artist - Исполнитель&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%title - Название&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%album - Альбом&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%track - Номер записи&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%time - Длина записи&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%file - Имя файла&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans&apos;;&quot;&gt;%uri - Полный путь к файлу&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location line="-17"/>
+        <source>Now playing: %artist - %title</source>
+        <translation type="unfinished">В колонках гремит: %artist - %title</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Changes message of &quot;Listening to music&quot; status</source>
+        <translation type="unfinished">Изменять сообщение статуса &quot;Слушаю музыку&quot;</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>%artist - %title</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JabberSettings</name>
+    <message>
+        <location filename="../../src/jabbersettings.ui" line="+14"/>
+        <source>Jabber</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Current mode</source>
+        <translation type="unfinished">Текущий режим</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Deactivated</source>
+        <translation type="unfinished">Отключен</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Activated</source>
+        <translation type="unfinished">Включен</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Info to be presented</source>
+        <translation type="unfinished">Что показывать</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Track number</source>
+        <translation>Номер записи</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Title</source>
+        <translation>Название записи</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Track length</source>
+        <translation>Длина записи</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Path to file</source>
+        <translation>Путь к файлу</translation>
     </message>
 </context>
 <context>
     <name>NowPlaying</name>
     <message>
-        <location line="+38"/>
+        <location filename="../../src/nowplaying.cpp" line="+38"/>
         <source>Now Playing</source>
         <translation type="unfinished"></translation>
     </message>
@@ -33,9 +161,82 @@
     </message>
 </context>
 <context>
+    <name>NowPlayingSettings</name>
+    <message>
+        <location filename="../../src/nowplayingsettings.ui" line="+17"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Mode</source>
+        <translation>Режим</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Current plugin status:</source>
+        <translation type="unfinished">Текущее состояние плагина:</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Is working</source>
+        <translation type="unfinished">работает</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Stop</source>
+        <translation type="unfinished">Остановить</translation>
+    </message>
+    <message>
+        <location line="+27"/>
+        <source>Set mode for all accounts</source>
+        <translation type="unfinished">Установить для всех аккаунтов</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>You have no Oscar, Jabber or MRIM accounts.</source>
+        <translation type="unfinished">У вас нет учетных записей ICQ/Oscar, XMPP/jabber или Mail.ru агента.</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Music Player</source>
+        <translation type="unfinished">Музыкальный плеер</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Hostname</source>
+        <translation type="unfinished">Имя  хоста</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>127.0.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Port</source>
+        <translation>Порт</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>6600</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+</context>
+<context>
     <name>Plugin</name>
     <message>
-        <location line="-183"/>
+        <location filename="../../src/nowplaying.cpp" line="-183"/>
         <source>Now Playing</source>
         <translation type="unfinished"></translation>
     </message>
@@ -67,7 +268,7 @@
 <context>
     <name>qutim_sdk_0_3::nowplaying::SettingsUI</name>
     <message>
-        <location filename="../../plugins/nowplaying/src/settingsui.cpp" line="+132"/>
+        <location filename="../../src/settingsui.cpp" line="+132"/>
         <source>working</source>
         <translation type="unfinished">работает</translation>
     </message>
