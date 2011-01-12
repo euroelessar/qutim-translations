d0ebb77073ab7f7eeaf53fe55a1063c82166c763
MrFree <f1.mrfree@gmail.com>
2011-01-12 22:25:20 +1000

    RU Добавлен астральный протокол, работающий через телепатию

---
+++ ru_RU/astral.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../src/astralplugin.cpp" line="33"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../src/astralplugin.cpp" line="30"/>
+        <source>Astral</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/astralplugin.cpp" line="31"/>
+        <source>Protocol support by Telepathy</source>
+        <translation type="unfinished">Протокол использующий Телепатию</translation>
+    </message>
+    <message>
+        <location filename="../../src/astralplugin.cpp" line="54"/>
+        <source>Plugin support by Telepathy</source>
+        <translation type="unfinished">Модуль использует Телепатию</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../src/astralplugin.cpp" line="34"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
