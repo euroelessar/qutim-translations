e2b0cb9bf7b59ac63357374a4c33e0d4f7ea38df
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-01-17 13:28:45 +0200

    BG: Added Astral

---
+++ bg_BG/astral.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../qutim-meta/protocols/astral/src/astralplugin.cpp" line="+33"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation>Руслан Нигматуллин</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="-3"/>
+        <source>Astral</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Protocol support by Telepathy</source>
+        <translation>Поддръжка на протоколи чрез Telepathy</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Plugin support by Telepathy</source>
+        <translation>Поддръжка на добавки чрез Telepathy</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-20"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
