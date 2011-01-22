673757b1c1c74373e4dd0535c683679ef234adbf
MrFree <f1.mrfree@gmail.com>
2011-01-22 22:32:31 +1000

    ru 2

---
+++ ru_RU/floaties.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../plugins/floaties/src/floaties.cpp" line="28"/>
+        <source>Ruslan Nigmatullin</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../plugins/floaties/src/floaties.cpp" line="31"/>
+        <source>Floaties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/floaties/src/floaties.cpp" line="32"/>
+        <source>Implementation of floaty contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../plugins/floaties/src/floaties.cpp" line="29"/>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
