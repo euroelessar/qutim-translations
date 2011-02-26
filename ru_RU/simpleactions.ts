44dac55e8570943f3c181202a81e2321ebcc8a79
MrFree <f1.mrfree@gmail.com>
2011-02-26 11:10:42 +1000

    RU add simpleactions.ts

---
+++ ru_RU/simpleactions.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ContactList</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="31"/>
+        <source>Edit tags</source>
+        <translation>Изменить теги</translation>
+    </message>
+</context>
+<context>
+    <name>Core::SimpleTagsEditor</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpletagseditor/simpletagseditor.cpp" line="17"/>
+        <source>Edit tags for %1</source>
+        <translation type="unfinished">Изменить метки у %1</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleTagsEditor</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpletagseditor/simpletagseditor.ui" line="46"/>
+        <source>Add</source>
+        <translation type="unfinished">Добавить</translation>
+    </message>
+</context>
+</TS>
