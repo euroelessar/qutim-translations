956b4937aa93a5a0c845b947349614ab45ba0b30
MrFree <f1.mrfree@gmail.com>
2011-02-26 23:18:34 +1000

    RU simplebox + qph

--- ru_RU/simpleactions.ts
+++ ru_RU/simpleactions.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="63"/>
         <source>Remove from roster</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Удалить из ростера</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="64"/>
         <source>Add to roster</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Добавить в ростер</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="112"/>
         <source>Unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Недоступно</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="213"/>
         <source>Remove contact</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Удалить контакт</translation>
     </message>
 </context>
 <context>
@@ -29,18 +29,18 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="46"/>
         <source>Show information</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Показать информацию</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="48"/>
         <source>Edit information</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Изменить информацию</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="51"/>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="100"/>
         <source>Information unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Информация недоступна</translation>
     </message>
 </context>
 <context>
@@ -48,7 +48,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="74"/>
         <source>Copy id to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Копировать id в буфер обмена</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="83"/>
@@ -58,7 +58,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="94"/>
         <source>Rename contact</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Переименовать контакт</translation>
     </message>
 </context>
 <context>
@@ -66,17 +66,17 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="163"/>
         <source>Rename contact %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Переименовать контакт %1</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="164"/>
         <source>Input new name for contact %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Дайте новое имя контакту %1</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="214"/>
         <source>Are you sure you want to delete a contact %1 from the roster?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Вы действительно хотите удалить %1 из ростера?</translation>
     </message>
 </context>
 <context>
@@ -92,7 +92,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="144"/>
         <source>Copy %1 to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Копировать %1 в буфер обмена</translation>
     </message>
 </context>
 <context>
