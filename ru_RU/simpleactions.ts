21bddc42b5810c7be9eb0e91fd248007b7bbd063
MrFree <f1.mrfree@gmail.com>
2011-02-27 16:43:39 +1000

    RU core+simpleactions+qph

--- ru_RU/simpleactions.ts
+++ ru_RU/simpleactions.ts
@@ -6,22 +6,30 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="63"/>
         <source>Remove from roster</source>
-        <translation type="unfinished">Удалить из ростера</translation>
+        <translation>Удалить из ростера</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="64"/>
         <source>Add to roster</source>
-        <translation type="unfinished">Добавить в ростер</translation>
+        <translation>Добавить в ростер</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="112"/>
         <source>Unavailable</source>
-        <translation type="unfinished">Недоступно</translation>
+        <translation>Недоступно</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="213"/>
         <source>Remove contact</source>
-        <translation type="unfinished">Удалить контакт</translation>
+        <translation>Удалить контакт</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactionsplugin.cpp" line="33"/>
+        <source>Aleksey Sidorov</source>
+        <translation>Алексей Сидоров</translation>
     </message>
 </context>
 <context>
@@ -29,18 +37,18 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="46"/>
         <source>Show information</source>
-        <translation type="unfinished">Показать информацию</translation>
+        <translation>Показать информацию</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="48"/>
         <source>Edit information</source>
-        <translation type="unfinished">Изменить информацию</translation>
+        <translation>Изменить информацию</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="51"/>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="100"/>
         <source>Information unavailable</source>
-        <translation type="unfinished">Информация недоступна</translation>
+        <translation>Информация недоступна</translation>
     </message>
 </context>
 <context>
@@ -48,7 +56,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="74"/>
         <source>Copy id to clipboard</source>
-        <translation type="unfinished">Копировать id в буфер обмена</translation>
+        <translation>Копировать id в буфер обмена</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="83"/>
@@ -58,7 +66,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="94"/>
         <source>Rename contact</source>
-        <translation type="unfinished">Переименовать контакт</translation>
+        <translation>Переименовать контакт</translation>
     </message>
 </context>
 <context>
@@ -66,17 +74,17 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="163"/>
         <source>Rename contact %1</source>
-        <translation type="unfinished">Переименовать контакт %1</translation>
+        <translation>Переименовать контакт %1</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="164"/>
         <source>Input new name for contact %1</source>
-        <translation type="unfinished">Дайте новое имя контакту %1</translation>
+        <translation>Дайте новое имя контакту %1</translation>
     </message>
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="214"/>
         <source>Are you sure you want to delete a contact %1 from the roster?</source>
-        <translation type="unfinished">Вы действительно хотите удалить %1 из ростера?</translation>
+        <translation>Вы действительно хотите удалить %1 из ростера?</translation>
     </message>
 </context>
 <context>
@@ -84,7 +92,20 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpletagseditor/simpletagseditor.cpp" line="17"/>
         <source>Edit tags for %1</source>
-        <translation type="unfinished">Изменить метки у %1</translation>
+        <translation>Изменить метки у %1</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactionsplugin.cpp" line="30"/>
+        <source>Simple Actions</source>
+        <translation>Простые действия</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactionsplugin.cpp" line="31"/>
+        <source>Default actionset for contacts</source>
+        <translation>Простые действия для контактов</translation>
     </message>
 </context>
 <context>
@@ -92,7 +113,7 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactions.cpp" line="144"/>
         <source>Copy %1 to clipboard</source>
-        <translation type="unfinished">Копировать %1 в буфер обмена</translation>
+        <translation>Копировать %1 в буфер обмена</translation>
     </message>
 </context>
 <context>
@@ -100,7 +121,15 @@
     <message>
         <location filename="../../qutim/src/corelayers/simpleactions/src/simpletagseditor/simpletagseditor.ui" line="46"/>
         <source>Add</source>
-        <translation type="unfinished">Добавить</translation>
+        <translation>Добавить</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../qutim/src/corelayers/simpleactions/src/simpleactionsplugin.cpp" line="34"/>
+        <source>Author</source>
+        <translation>Автор</translation>
     </message>
 </context>
 </TS>
