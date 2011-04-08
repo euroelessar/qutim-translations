c61daa59a79be1a3a3248032a748a0a21c4157d0
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-04-08 19:58:20 +0300

    BG: Package Manager

---
+++ bg_BG/package_manager.ts
@@ -0,0 +1,509 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>ChooseCategoryPage</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/tools/packgen/choosecategorypage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Package category:</source>
+        <translation>Категория на пакета:</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Art</source>
+        <translation>Графика</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Core</source>
+        <translation>Ядро</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Lib</source>
+        <translation>Библиотека</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Plugin</source>
+        <translation>Добавка</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/tools/packgen/choosecategorypage.cpp" line="+42"/>
+        <source>Select art</source>
+        <translation>Избор на графика</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Select core</source>
+        <translation>Избор на ядро</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Library (*.dll)</source>
+        <translation>Библиотека (*.dll)</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Library (*.dylib *.bundle *.so)</source>
+        <translation>Библиотека (*.dylib *.bundle *.so)</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Library (*.so)</source>
+        <translation>Библиотека (*.so)</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Select library</source>
+        <translation>Избор на библиотека</translation>
+    </message>
+</context>
+<context>
+    <name>ChoosePathPage</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/tools/packgen/choosepathpage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigPackagePage</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/tools/packgen/configpackagepage.ui" line="+14"/>
+        <source>WizardPage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Name:</source>
+        <translation>Име:</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <location line="+21"/>
+        <location line="+51"/>
+        <location line="+21"/>
+        <location line="+31"/>
+        <source>*</source>
+        <translation>*</translation>
+    </message>
+    <message>
+        <location line="-115"/>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Category:</source>
+        <translation>Категория:</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Art</source>
+        <translation>Графика</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Core</source>
+        <translation>Ядро</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Lib</source>
+        <translation>Библиотека</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Plugin</source>
+        <translation>Добавка</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Short description:</source>
+        <translation>Кратко описание</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Url:</source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Author:</source>
+        <translation>Автор:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>License:</source>
+        <translation>Лиценз:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Platform:</source>
+        <translation>Платформа:</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Package name:</source>
+        <translation>Име на пакета:</translation>
+    </message>
+</context>
+<context>
+    <name>PackageManager</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/src/plugman.cpp" line="+38"/>
+        <source>Manage packages</source>
+        <translation>Управление на пакети</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="-11"/>
+        <source>Package manager</source>
+        <translation>Управление на пакети</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Provides possibility to Get Hot New Stuff from open community</source>
+        <translation>Дава възможност за получаване на нови &quot;горещи&quot; неща от отвореното общество</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugpackage.cpp" line="+35"/>
+        <source>Package name is empty</source>
+        <translatorcomment> </translatorcomment>
+        <translation>Името на пакета е празно</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Package type is empty</source>
+        <translation>Типът на пакета е празен</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Invalid package version</source>
+        <translation>Невалидна версия на пакета</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Wrong platform</source>
+        <translation>Неправилна платформа</translation>
+    </message>
+</context>
+<context>
+    <name>manager</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/ui/manager.ui" line="+20"/>
+        <source>Plugman</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>Not yet implemented</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>find</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugDownloader</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugdownloader.cpp" line="+92"/>
+        <source>bytes/sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>kB/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>MB/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Downloading: %1%, speed: %2 %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugInstaller</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/pluginstaller.cpp" line="+60"/>
+        <source>Need restart!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Unable to open archive: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <location line="+135"/>
+        <source>warning: trying to overwrite existing files!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-129"/>
+        <source>Unable to extract archive: %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <location line="+134"/>
+        <location line="+10"/>
+        <source>Installing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-114"/>
+        <source>Invalid package: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <location line="+9"/>
+        <source>Removing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+53"/>
+        <source>Unable to update package %1: installed version is later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Unable to install package: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugItemDelegate</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugitemdelegate.cpp" line="+68"/>
+        <source>isUpgradable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>isInstallable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>isDowngradable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Upgrade</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugManager</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugmanager.cpp" line="+45"/>
+        <source>Actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Update packages list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Upgrade all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Revert changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugPackageModel</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugpackagemodel.cpp" line="+98"/>
+        <source>Packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugXMLHandler</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugxmlhandler.cpp" line="+118"/>
+        <location line="+106"/>
+        <location line="+15"/>
+        <source>Unable to open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-117"/>
+        <location line="+106"/>
+        <source>Unable to set content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-92"/>
+        <location line="+39"/>
+        <source>Unable to write file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-22"/>
+        <location line="+135"/>
+        <source>Can&apos;t read database. Check your pesmissions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-130"/>
+        <location line="+136"/>
+        <source>Broken package database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-98"/>
+        <source>unable to open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>unable to set content</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>plugmanSettings</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/ui/settings.ui" line="+20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>group packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Mirror list&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Not yet implemented</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Verdana&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt;&quot;&gt;simple qutIM extentions manager.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:600;&quot;&gt;Author: &lt;/span&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt;&quot;&gt;Sidorov Aleksey&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; font-weight:600;&quot;&gt;Contacts: &lt;/span&gt;&lt;a href=&quot;mailto::sauron@citadelspb.com&quot;&gt;&lt;span style=&quot; font-family:&apos;Bitstream Vera Sans&apos;; font-size:10pt; text-decoration: underline; color:#0000ff;&quot;&gt;sauron@citadeslpb.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Droid Serif&apos;; font-size:10pt;&quot;&gt;&lt;br /&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Droid Serif&apos;; font-size:10pt;&quot;&gt;2008-2009&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qutim-meta/plugins/plugman/oldsrc/plugmansettings.cpp" line="+18"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Url</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
