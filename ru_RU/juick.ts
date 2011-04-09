09b2c5fffcde73015d6cbefc30922cce46065523
MrFree <f1.mrfree@gmail.com>
2011-04-09 14:40:50 +1100

    RU add juick

---
+++ ru_RU/juick.ts
@@ -0,0 +1,215 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../juick-plugin/src/juick.cpp" line="18"/>
+        <source>Stanislav (proDOOMman) Kosolapov</source>
+        <translation type="unfinished">Косолапов (proDOOMman) Станислав</translation>
+    </message>
+</context>
+<context>
+    <name>Juick</name>
+    <message>
+        <location filename="../../juick-plugin/src/juick.cpp" line="31"/>
+        <source>Juick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JuickSettings</name>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="14"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="140"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="20"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="141"/>
+        <source>@user_id</source>
+        <translation type="unfinished">@username</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="29"/>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="61"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="142"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="146"/>
+        <source>Bold</source>
+        <translation type="unfinished">Жирно</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="36"/>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="68"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="143"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="147"/>
+        <source>Italic</source>
+        <translation type="unfinished">курсив</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="43"/>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="75"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="144"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="148"/>
+        <source>Underline</source>
+        <translation type="unfinished">Подчеркивание</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="52"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="145"/>
+        <source>#post_id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="84"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="149"/>
+        <source>Show additional commands</source>
+        <translation type="unfinished">Показывать дополнительные команды</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="94"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="150"/>
+        <source>User commands</source>
+        <translation type="unfinished">Команды на @username</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="101"/>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="115"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="152"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="156"/>
+        <source>Commands separated by space</source>
+        <translation type="unfinished">Команды разделяются пробелом</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juicksettings.ui" line="108"/>
+        <location filename="../../juick-plugin/build/ui_juicksettings.h" line="154"/>
+        <source>Post commands</source>
+        <translation type="unfinished">Команды на #postid</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../juick-plugin/src/juick.cpp" line="15"/>
+        <source>Juick</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/juick.cpp" line="16"/>
+        <source>Some integration with microblogging juick.com</source>
+        <translation type="unfinished">Жуйкоплагин, если не знаете что такое - вырубайте - оно е нужно</translation>
+    </message>
+</context>
+<context>
+    <name>QtColorComboBox</name>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="131"/>
+        <source>More...</source>
+        <translation>Еще...</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="232"/>
+        <source>User color %1</source>
+        <translation type="unfinished">Свой цвет %1</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="276"/>
+        <source>User Color 99</source>
+        <translation type="unfinished">Свой цвет 99</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="290"/>
+        <source>Black</source>
+        <translation>Черный</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="291"/>
+        <source>White</source>
+        <translation>Белый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="292"/>
+        <source>Red</source>
+        <translation>Красный</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="293"/>
+        <source>Dark red</source>
+        <translation>Темно-красный</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="294"/>
+        <source>Green</source>
+        <translation>Зеленый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="295"/>
+        <source>Dark green</source>
+        <translation>Темно-зеленый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="296"/>
+        <source>Blue</source>
+        <translation>Синий</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="297"/>
+        <source>Dark blue</source>
+        <translation>Темно-синий</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="298"/>
+        <source>Cyan</source>
+        <translation>Циан</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="299"/>
+        <source>Dark cyan</source>
+        <translation>Темно-циан</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="300"/>
+        <source>Magenta</source>
+        <translation>Мажента</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="301"/>
+        <source>Dark magenta</source>
+        <translation>Темн-мажента</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="302"/>
+        <source>Yellow</source>
+        <translation>Желтый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="303"/>
+        <source>Dark yellow</source>
+        <translation>Темно-желтый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="304"/>
+        <source>Gray</source>
+        <translation>Серый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="305"/>
+        <source>Dark gray</source>
+        <translation>Темно-серый</translation>
+    </message>
+    <message>
+        <location filename="../../juick-plugin/src/qtcolorcombobox.cpp" line="306"/>
+        <source>Light gray</source>
+        <translation type="unfinished">Светло-серый</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../juick-plugin/src/juick.cpp" line="19"/>
+        <source>Author</source>
+        <translation type="unfinished">Автор</translation>
+    </message>
+</context>
+</TS>
