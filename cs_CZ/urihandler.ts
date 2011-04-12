94a8c656fba6c13787a0452a0cfad622a9276cdd
Lms <lms.cze7@gmail.com>
2011-04-12 14:56:29 +0200

    cs_CZ: added urihandler

---
+++ cs_CZ/urihandler.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_GB">
+<context>
+    <name>AccSelectionDlg</name>
+    <message>
+        <location filename="plugins/urihandler/src/account_selection.ui" line="14"/>
+        <source>Select account</source>
+        <translation>Výběr účtu</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="plugins/urihandler/src/urihandlerplugin.cpp" line="10"/>
+        <source>Vizir Ivan</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="plugins/urihandler/src/urihandlerplugin.cpp" line="13"/>
+        <source>Uri Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/urihandlerplugin.cpp" line="14"/>
+        <source>Allows qutIM to open link of some types.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="40"/>
+        <source>link is invalid or empty.</source>
+        <translation type="unfinished">odkaz je neplatný nebo prázdný.</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="43"/>
+        <source>no accounts to open link for.</source>
+        <translation type="unfinished">žádné účty pro otevření odkazu.</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="47"/>
+        <source>unknown error.</source>
+        <translation type="unfinished">neznámá chyba-</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="51"/>
+        <source>Cannot open link: %1.</source>
+        <translation type="unfinished">Nelze otevřít odkaz: %1.</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="63"/>
+        <source>Please, select account to send message to %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="66"/>
+        <source>Please, select account to add %1 to roster.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="69"/>
+        <source>Please, select account to remove %1 from roster of that account.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="72"/>
+        <source>Please, select account to subscribe to %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="75"/>
+        <source>Please, select account to unsubscribe from %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="78"/>
+        <source>Please, select account.</source>
+        <translation type="unfinished">Prosím vyberte účet.</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="80"/>
+        <source>Note</source>
+        <translation type="unfinished">Poznámka</translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/abstract_handler.cpp" line="80"/>
+        <source>Action was not specified by link, so qutIM will simply open a chat with %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/handler_xmpp.cpp" line="61"/>
+        <location filename="plugins/urihandler/subplugins/urihandler_jabber/handler_xmpp.cpp" line="61"/>
+        <source>Name of contact is being added</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/handler_xmpp.cpp" line="63"/>
+        <location filename="plugins/urihandler/subplugins/urihandler_jabber/handler_xmpp.cpp" line="63"/>
+        <source>Group of contact is being added</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="plugins/urihandler/src/handler_xmpp.cpp" line="71"/>
+        <location filename="plugins/urihandler/subplugins/urihandler_jabber/handler_xmpp.cpp" line="71"/>
+        <source>Message to send</source>
+        <translation type="unfinished">Zpráva k odeslání</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="plugins/urihandler/src/urihandlerplugin.cpp" line="11"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+</context>
+</TS>
