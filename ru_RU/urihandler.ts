7f0109ce394c2baef1d577369eb7d0ef254b669a
MrFree <f1.mrfree@gmail.com>
2011-04-13 23:26:58 +1100

    RU urihandler

---
+++ ru_RU/urihandler.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AccSelectionDlg</name>
+    <message>
+        <location filename="src/account_selection.ui" line="+14"/>
+        <source>Select account</source>
+        <translation type="unfinished">Выберите аккаунт</translation>
+    </message>
+</context>
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="src/urihandlerplugin.cpp" line="+10"/>
+        <source>Vizir Ivan</source>
+        <translation>Визир Иван</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location line="+3"/>
+        <source>Uri Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Allows qutIM to open link of some types.</source>
+        <translation type="unfinished">Плагин позволяет открывать некоторые типы ссылок в qutIM.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/abstract_handler.cpp" line="+40"/>
+        <source>link is invalid or empty.</source>
+        <translation type="unfinished">ссылка не верная или пуста.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>no accounts to open link for.</source>
+        <translation type="unfinished">нет аккаунта ля открытия ссылки.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>unknown error.</source>
+        <translation type="unfinished">неизвестная ошибка.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Cannot open link: %1.</source>
+        <translation type="unfinished">Не получилось открыть ссылку: %1.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Please, select account to send message to %1.</source>
+        <translation type="unfinished">Укажите аккаунт для отправки сообщения %1.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Please, select account to add %1 to roster.</source>
+        <translation type="unfinished">Выберите аккаунт для добавления %1 в ростер.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Please, select account to remove %1 from roster of that account.</source>
+        <translation type="unfinished">Укажие аккаунт из ростера котоnого удалить %1.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Please, select account to subscribe to %1.</source>
+        <translation type="unfinished">Укажите аккаунт для подписки на %1.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Please, select account to unsubscribe from %1.</source>
+        <translation type="unfinished">Укажите аккаунт для отдписки от %1.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Please, select account.</source>
+        <translation type="unfinished">Выберите аккаунт.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Note</source>
+        <translation type="unfinished">Заметка</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Action was not specified by link, so qutIM will simply open a chat with %1.</source>
+        <translation type="unfinished">Действие не указан в ссылке, поэтому просто открываем чат с %1.</translation>
+    </message>
+    <message>
+        <location filename="src/handler_xmpp.cpp" line="+61"/>
+        <location filename="subplugins/urihandler_jabber/handler_xmpp.cpp" line="+61"/>
+        <source>Name of contact is being added</source>
+        <translation type="unfinished">Имя добавляемого контакта</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="subplugins/urihandler_jabber/handler_xmpp.cpp" line="+2"/>
+        <source>Group of contact is being added</source>
+        <translation type="unfinished">Группа добавляемого контакта</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <location filename="subplugins/urihandler_jabber/handler_xmpp.cpp" line="+8"/>
+        <source>Message to send</source>
+        <translation type="unfinished">Сообщение для отправки</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="src/urihandlerplugin.cpp" line="-3"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
