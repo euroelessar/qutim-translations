6be8b7aa23b81448c6039ac37bad9245cb870ce0
MrFree <f1.mrfree@gmail.com>
2011-04-13 21:56:31 +1100

    RU wintegration

--- ru_RU/win7int2.ts
+++ ru_RU/win7int2.ts
@@ -4,7 +4,7 @@
 <context>
     <name>Author</name>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="28"/>
+        <location filename="src/winint.cpp" line="+38"/>
         <source>Vizir Ivan</source>
         <translation type="unfinished">Визир Иван</translation>
     </message>
@@ -12,20 +12,48 @@
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="31"/>
         <source>Windows 7 Integration</source>
-        <translation>Интеграция с Windows7®©™</translation>
+        <translation type="obsolete">Интеграция с Windows7®©™</translation>
     </message>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="32"/>
-        <source>/* TODO */</source>
+        <location line="+3"/>
+        <location line="+12"/>
+        <source>Windows Integration</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-11"/>
+        <source>Adds count of unread messages as an icon to taskbar button of qutim, along with some commands list and provides a bit more usable notification area icon than default plugin.</source>
+        <translation type="unfinished">Добавляет взаимодействие с супербаром windows, количество непрочитанных сообщений, команды, уведомления итп.</translation>
+    </message>
+</context>
+<context>
+    <name>SelectAccDlg</name>
+    <message>
+        <location filename="src/selectaccdlg.ui" line="+14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Диалог</translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Select your account</source>
+        <translation type="unfinished">Выберите аккаунт</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Please, select one of your accout to chat with Jabber ID %1.</source>
+        <translation type="unfinished">Выберите аккаун jabber, для часа с %1.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Available accounts:</source>
+        <translation type="unfinished">Доступные аккаунты:</translation>
     </message>
 </context>
 <context>
     <name>Task</name>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="29"/>
+        <location filename="src/winint.cpp" line="-3"/>
         <source>Author</source>
         <translation>Автор</translation>
     </message>
@@ -33,12 +61,103 @@
 <context>
     <name>WPreviews</name>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="245"/>
         <source>&lt;b&gt;You have new messages:&lt;/b&gt;</source>
-        <translation type="unfinished">&lt;b&gt;Новые сообщения:&lt;/b&gt;</translation>
+        <translation type="obsolete">&lt;b&gt;Новые сообщения:&lt;/b&gt;</translation>
     </message>
     <message numerus="yes">
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="246"/>
+        <source>&amp;middot; %n message(s) from chats.</source>
+        <translation type="obsolete">
+            <numerusform>&amp;middot; %n сообщение из чата.</numerusform>
+            <numerusform>&amp;middot; %n сообщения из чатов.</numerusform>
+            <numerusform>&amp;middot; %n сообщений из чатов.</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>&amp;middot; %n message(s) from conferences.</source>
+        <translation type="obsolete">
+            <numerusform>&amp;middot; %n сообщение из конференции.</numerusform>
+            <numerusform>&amp;middot; %n сообщения из конференций.</numerusform>
+            <numerusform>&amp;middot; %n сообщений из конференций.</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Last received from:</source>
+        <translation type="obsolete">Последний принят от:</translation>
+    </message>
+    <message>
+        <source>You have no new messages.</source>
+        <translation type="obsolete">Нет новых сообщений.</translation>
+    </message>
+</context>
+<context>
+    <name>WSettingsForm</name>
+    <message>
+        <location filename="src/wsettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>General</source>
+        <translation>Главные</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Links association</source>
+        <translation type="unfinished">Ассоциации ссылок</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Open Jabber links with qutIM. This may require administrative privelegies.</source>
+        <translation type="unfinished">Открытие jabber ссылок в qutIM. Это может поьтребовать административных привелегий.</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Update associations</source>
+        <translation type="unfinished">Обновить ассоциации</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>Taskbar</source>
+        <translation type="unfinished">Панель задач</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Overlay Icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <location line="+36"/>
+        <source>Enabled</source>
+        <translation type="unfinished">Включено</translation>
+    </message>
+    <message>
+        <location line="-26"/>
+        <location line="+36"/>
+        <source>Show new messages number</source>
+        <translation type="unfinished">Показывать количество сообщений</translation>
+    </message>
+    <message>
+        <location line="-26"/>
+        <source>Add new conference messages number</source>
+        <translation type="unfinished">Добавить число сообщений из конференций</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Thumbnails</source>
+        <translation type="unfinished">Миниятюры</translation>
+    </message>
+</context>
+<context>
+    <name>WThumbnailsProvider</name>
+    <message>
+        <location filename="subplugins/win7taskbar/thumbnails-renderer.cpp" line="+91"/>
+        <source>&lt;b&gt;You have new messages.&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;У тебя новые  сообщения.&lt;/b&gt;</translation>
+    </message>
+    <message numerus="yes">
+        <location line="+3"/>
         <source>&amp;middot; %n message(s) from chats.</source>
         <translation type="unfinished">
             <numerusform>&amp;middot; %n сообщение из чата.</numerusform>
@@ -47,7 +166,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="247"/>
+        <location line="+1"/>
         <source>&amp;middot; %n message(s) from conferences.</source>
         <translation type="unfinished">
             <numerusform>&amp;middot; %n сообщение из конференции.</numerusform>
@@ -56,12 +175,12 @@
         </translation>
     </message>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="248"/>
+        <location line="+6"/>
         <source>Last received from:</source>
         <translation type="unfinished">Последний принят от:</translation>
     </message>
     <message>
-        <location filename="../../plugins/win-integration/src/win7int.cpp" line="257"/>
+        <location line="+4"/>
         <source>You have no new messages.</source>
         <translation type="unfinished">Нет новых сообщений.</translation>
     </message>
