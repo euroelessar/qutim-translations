86f274d1d22278b6626f68062c4cd9f52d1f18d7
MrFree <f1.mrfree@gmail.com>
2011-04-16 16:43:47 +1100

    RU antispamwtf?

--- ru_RU/antispam.ts
+++ ru_RU/antispam.ts
@@ -2,9 +2,79 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>Antispam</name>
+    <message>
+        <location filename="../../plugins/antispam/src/antispamplugin.cpp" line="+18"/>
+        <source>Antispam</source>
+        <translation type="unfinished">Антиспам</translation>
+    </message>
+</context>
+<context>
+    <name>Antispam::Handler</name>
+    <message>
+        <location filename="../../plugins/antispam/src/handler.cpp" line="+42"/>
+        <source>Beer, wine, vodka, champagne: after which drink in this sequence I should stop?</source>
+        <translation type="unfinished">Пиво, вино, водка, шампусик: после чего из списка мне стоит прекратить на сегодня пить?</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>We are ready to drink with you!</source>
+        <translation type="unfinished">Мы готовы выпить с тобой, о юный падаван!</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>vodka;Vodka</source>
+        <translation type="unfinished">vodka;Vodka;водка;Водка;wodka;Wodka</translation>
+    </message>
+</context>
+<context>
+    <name>Antispam::SettingsWidget</name>
+    <message>
+        <location filename="../../plugins/antispam/src/settingswidget.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Enabled</source>
+        <translation type="unfinished">Включено</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Question:</source>
+        <translation type="unfinished">Вопрос:</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Answers (semicolon as a separator):</source>
+        <translatorcomment>а в примере ;</translatorcomment>
+        <translation type="unfinished">Ответы (разделять запятыми):</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Message on correct answer:</source>
+        <translation type="unfinished">Сообщение о правильмо ответе:</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/antispam/src/settingswidget.cpp" line="+46"/>
+        <source>Beer, wine, vodka, champagne: after which drink in this sequence I should stop?</source>
+        <translation>Пиво, вино, водка, шампусик: после чего из списка мне стоит прекратить на сегодня пить?</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>We are ready to drink with you!</source>
+        <translation>Мы готовы выпить с тобой, о юный падаван!</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>vodka;Vodka</source>
+        <translation>vodka;Vodka;водка;Водка;wodka;Wodka</translation>
+    </message>
+</context>
+<context>
     <name>Author</name>
     <message>
-        <location filename="../../plugins/antispam/src/antispamplugin.cpp" line="+13"/>
+        <location filename="../../plugins/antispam/src/antispamplugin.cpp" line="+7"/>
         <source>Sidorov Aleksey</source>
         <translation>Сидоров Алексей</translation>
     </message>
@@ -23,9 +93,17 @@
     </message>
 </context>
 <context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../plugins/antispam/src/handler.cpp" line="+27"/>
+        <source>Message from %1 blocked  on suspicion of spam.</source>
+        <translation type="unfinished">Сообщение от %1 заблокировано антиспамом.</translation>
+    </message>
+</context>
+<context>
     <name>Task</name>
     <message>
-        <location line="-4"/>
+        <location filename="../../plugins/antispam/src/antispamplugin.cpp" line="-4"/>
         <source>Author</source>
         <translation>Автор</translation>
     </message>
