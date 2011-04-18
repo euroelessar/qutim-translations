be152642ab24ab6c3be856f70c8235624b401c2c
Lms <lms.cze7@gmail.com>
2011-04-18 09:43:16 +0200

    cs-CZ: symbian integration

---
+++ cs_CZ/symbian-integration.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_GB">
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <source>Toggle fullscreen</source>
+        <translation>Přepnout na celou obrazovku</translation>
+    </message>
+</context>
+<context>
+    <name>Core::AdiumChat::Symbian::StackedChatWidget</name>
+    <message>
+        <source>Send to</source>
+        <translation>Odeslat do</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Odeslat</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Akce</translation>
+    </message>
+</context>
+<context>
+    <name>Core::SimpleContactList::SymbianWidget</name>
+    <message>
+        <source>Search contact</source>
+        <translation>Hledat kontakt</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Stav</translation>
+    </message>
+    <message>
+        <source>Set Status Text</source>
+        <translation>Nastavit text stavu</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Akce</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Chat</source>
+        <translation>Konverzace</translation>
+    </message>
+</context>
+<context>
+    <name>StackedChatBehavior</name>
+    <message>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Store service messages in history</source>
+        <translation>Ukládat servisní zprávy do historie</translation>
+    </message>
+    <message>
+        <source>Number of previous messages shown in new chats</source>
+        <translation>Počet předchozích zpráv zobrazovaných v nových konverzacích</translation>
+    </message>
+    <message>
+        <source>Number of shown previous messages:</source>
+        <translation>Počet zobrazovaných předchozích zpráv:</translation>
+    </message>
+    <message>
+        <source>Don&apos;t group messages after (sec):</source>
+        <translation>Neseskupovat zprávy po [s]:</translation>
+    </message>
+    <message>
+        <source>Notifications in active chat</source>
+        <translation>Oznamování v aktivní konverzaci</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <source>Author</source>
+        <translation type="obsolete">Autor</translation>
+    </message>
+</context>
+</TS>
