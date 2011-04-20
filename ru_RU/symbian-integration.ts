1eeffa941c4424130783fe81af4f41075f936a11
MrFree <f1.mrfree@gmail.com>
2011-04-21 00:09:04 +1100

    RU symbian-integration.ts

---
+++ ru_RU/symbian-integration.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <location filename="../../plugins/symbian-integration/symbianchatform/stackedchatwidget.cpp" line="+59"/>
+        <source>Toggle fullscreen</source>
+        <translation type="unfinished">Во весь экран</translation>
+    </message>
+</context>
+<context>
+    <name>Core::AdiumChat::Symbian::StackedChatWidget</name>
+    <message>
+        <location line="-22"/>
+        <source>Send to</source>
+        <translation type="unfinished">Отправить в</translation>
+    </message>
+    <message>
+        <location line="+71"/>
+        <location line="+197"/>
+        <source>Send</source>
+        <translation>Спамить</translation>
+    </message>
+    <message>
+        <location line="-18"/>
+        <location line="+17"/>
+        <source>Actions</source>
+        <translation>Действия</translation>
+    </message>
+</context>
+<context>
+    <name>Core::SimpleContactList::SymbianWidget</name>
+    <message>
+        <location filename="../../plugins/symbian-integration/symbiancontactlistwidget/symbianwidget.cpp" line="+67"/>
+        <source>Search contact</source>
+        <translation>Поиск контакта</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <location line="+48"/>
+        <source>Set Status Text</source>
+        <translation>Установить текст статуса</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Actions</source>
+        <translation>Действия</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../plugins/symbian-integration/symbianchatform/stackedchatform.cpp" line="+23"/>
+        <source>Chat</source>
+        <translation type="unfinished">Чат</translation>
+    </message>
+</context>
+<context>
+    <name>StackedChatBehavior</name>
+    <message>
+        <location filename="../../plugins/symbian-integration/symbianchatform/settings/stackedchatbehavior.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>Store service messages in history</source>
+        <translation type="unfinished">Хранить служебные сообщения в истории</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Number of previous messages shown in new chats</source>
+        <translation type="unfinished">Количество предыдущих сообщений в новом чатике</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Number of shown previous messages:</source>
+        <translation type="unfinished">Количество предыдущих сообщений:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Don&apos;t group messages after (sec):</source>
+        <translation type="unfinished">Не группировать сообщения после (с):</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Notifications in active chat</source>
+        <translation type="unfinished">Уведомления в активном чате</translation>
+    </message>
+</context>
+</TS>
