334227021fc517709cbf33cdf2b7acfe89d5cb96
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-04-20 12:07:10 +0300

    BG: Package-Manager renamed (back) to Plugman and Mobility added

---
+++ bg_BG/mobility.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/mobility/src/bearermanager/connectionmanager.cpp" line="+48"/>
+        <source>Sidorov Aleksey</source>
+        <translation>Алексей Сидоров</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionManager::ConnectionManager</name>
+    <message>
+        <location line="+74"/>
+        <source>within %1 seconds</source>
+        <translation>в рамките на %1 секунди</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>immediately</source>
+        <translation>моментално</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>%1 will be reconnected %2</source>
+        <translation>%1 ще се свърже повторно %2</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>ConnectionManager</source>
+        <translation>Управление на връзката</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerSettings</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/mobility/src/bearermanager/managersettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Autoconnect on startup</source>
+        <translation>Автоматично свързване при стартиране</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/mobility/src/bearermanager/connectionmanager.cpp" line="-87"/>
+        <source>ConnectionManager</source>
+        <translation>Управление на връзката</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Used to monitor the availability of network.</source>
+        <translation>Използва се за наблюдение на свързаността към мрежата.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location line="+22"/>
+        <source>Connection manager</source>
+        <translation>Управление на връзката</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-19"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
