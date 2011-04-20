1bdc93830d7bab0d0dbbc02421d29d7bd7f929b9
MrFree <f1.mrfree@gmail.com>
2011-04-20 20:52:14 +1100

    RU new mobility

---
+++ ru_RU/mobility.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../plugins/mobility/src/bearermanager/connectionmanager.cpp" line="+48"/>
+        <source>Sidorov Aleksey</source>
+        <translation>Сидоров Алексей</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionManager::ConnectionManager</name>
+    <message>
+        <location line="+74"/>
+        <source>within %1 seconds</source>
+        <translation type="unfinished">в %1 секунды(у)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>immediately</source>
+        <translation type="unfinished">немедленно</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>%1 will be reconnected %2</source>
+        <translation type="unfinished">%1 будет переподключен  %2</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>ConnectionManager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManagerSettings</name>
+    <message>
+        <location filename="../../plugins/mobility/src/bearermanager/managersettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Autoconnect on startup</source>
+        <translation type="unfinished">Автоподключение при запуске</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../plugins/mobility/src/bearermanager/connectionmanager.cpp" line="-87"/>
+        <source>ConnectionManager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Used to monitor the availability of network.</source>
+        <translation type="unfinished">Используется для отслеживания доступности сети.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location line="+22"/>
+        <source>Connection manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location line="-19"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+</TS>
