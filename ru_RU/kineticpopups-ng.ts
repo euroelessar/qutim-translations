27c8020f9c93b09a3fc6a70985b9b510efe96629
MrFree <f1.mrfree@gmail.com>
2011-04-27 07:52:20 +1100

    RU +kineticpopups-ng

---
+++ ru_RU/kineticpopups-ng.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>KineticPopups::Backend</name>
+    <message>
+        <location filename="../../plugins/kineticpopups-ng/src/backend.cpp" line="+31"/>
+        <source>Unable to create notification inside QtQuick</source>
+        <translation type="unfinished">Не удалось создать уведомление в QtQuick</translation>
+    </message>
+</context>
+<context>
+    <name>KineticPopups::QuickNotify</name>
+    <message>
+        <location filename="../../plugins/kineticpopups-ng/src/popupwidgets/quickpopupwidget.cpp" line="+58"/>
+        <source>Notification from qutIM</source>
+        <translation type="unfinished">Уведомление от qutIM</translation>
+    </message>
+</context>
+</TS>
