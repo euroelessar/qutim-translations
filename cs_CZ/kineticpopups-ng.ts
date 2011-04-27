e1da32d48b4b6f57a7ea85179d96a46345a64708
Lms <lms.cze7@gmail.com>
2011-04-27 07:06:03 +0200

    cs-CZ: added kineticpopups-ng

---
+++ cs_CZ/kineticpopups-ng.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_GB">
+<context>
+    <name>KineticPopups::Backend</name>
+    <message>
+        <source>Unable to create notification inside QtQuick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KineticPopups::QuickNotify</name>
+    <message>
+        <source>Notification from qutIM</source>
+        <translation>Oznámení z qutIMu</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <source>Author</source>
+        <translation type="obsolete">Autor</translation>
+    </message>
+</context>
+</TS>
