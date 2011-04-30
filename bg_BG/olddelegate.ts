08644a96b06233a8cc82403fce0368a489fe70b8
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-04-30 13:17:03 +0300

    BG: Added Old Delegate

---
+++ bg_BG/olddelegate.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>ContactList</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/olddelegate/contactlistitemdelegate.cpp" line="+55"/>
+        <source>ContactList</source>
+        <translation>Списък с контакти</translation>
+    </message>
+    <message>
+        <location filename="../../qutim-meta/plugins/olddelegate/settings/olddelegatesettings.cpp" line="+17"/>
+        <source>Default (depends on platform)</source>
+        <translation>Подразбиращ се (според платформата)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Small (16x16)</source>
+        <translation>Малък (16х16)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Medium (22x22)</source>
+        <translation>Среден (22х22)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Large (32x32)</source>
+        <translation>Голям (32х32)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Very large (48x48)</source>
+        <translation>Много голям (48х48)</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Huge (64x64)</source>
+        <translation>Огромен (64х64)</translation>
+    </message>
+</context>
+<context>
+    <name>OldDelegateSettings</name>
+    <message>
+        <location filename="../../qutim-meta/plugins/olddelegate/settings/olddelegatesettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Status icons size:</source>
+        <translation>Размер на иконите за статус:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Show avatars</source>
+        <translation>Да се показват аватари</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Show status text</source>
+        <translation>Да се показва текста на статуса</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Show extended icons</source>
+        <translation>Да се показват иконите на разширените статуси</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Theme:</source>
+        <translation>Тема:</translation>
+    </message>
+</context>
+</TS>
