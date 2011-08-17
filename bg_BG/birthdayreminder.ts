0d22738f6e2d6fc9cbb1a5350c2bea6768a572af
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-08-17 11:44:24 +0300

    BG: Added BirthdayReminder

---
+++ bg_BG/birthdayreminder.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>BirthdayReminder</name>
+    <message>
+        <location filename="../../qutim/plugins/birthdayreminder/src/birthdayreminder.cpp" line="+269"/>
+        <source>It&apos;s %1&apos;s birthday today! Hooray!</source>
+        <translation>%1 има рожден ден днес! Ура!</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>%1&apos;s birthday will be tomorrow! Hooray!</source>
+        <translation>%1 има рожден ден утре! Ура!</translation>
+    </message>
+    <message numerus="yes">
+        <location line="+3"/>
+        <source>%2&apos;s birthday is in %1 days</source>
+        <translation>
+            <numerusform>%2 има рожден след %1 ден</numerusform>
+            <numerusform>%2 има рожден след %1 дни</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>%1&apos;s birhday</source>
+        <translation>Рождeн ден на %1</translation>
+    </message>
+</context>
+<context>
+    <name>BirthdayReminderSettings</name>
+    <message>
+        <location filename="../../qutim/plugins/birthdayreminder/src/birthdayremindersettings.ui" line="+14"/>
+        <source>Birthday reminder settings</source>
+        <translation>Настройки за напомнянията за рождени дни</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Show notifications every</source>
+        <translation>Показване на уведомления на всеки</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>hours</source>
+        <translation>часа</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>starting from</source>
+        <translation>Започвайки от</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>days before birthday</source>
+        <translation>дни преди рождения ден</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../qutim/plugins/birthdayreminder/src/birthdayreminder.cpp" line="-143"/>
+        <source>BirthdayReminder</source>
+        <translation>Напомняне за рождени дни</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>This plugin shows notifications when someone from your contact list has a birthday</source>
+        <translation>Тази добавка уведомява когато някой от списъка с контакти има рожден ден</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location line="+21"/>
+        <source>Birthday reminder</source>
+        <translation>Напомняне за рождени дни</translation>
+    </message>
+</context>
+</TS>
