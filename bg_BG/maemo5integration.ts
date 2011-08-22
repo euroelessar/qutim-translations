fb36635b98a2cbaae95416a5a8e954b299f91aca
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-08-22 14:31:27 +0300

    Maemo 5 Intgration added

---
+++ bg_BG/maemo5integration.ts
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>Author</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5led/maemo5ledplugin.cpp" line="+41"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settingsplugin.cpp" line="+39"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5tray/maemo5trayplugin.cpp" line="+25"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5vibra/maemo5vibraplugin.cpp" line="+41"/>
+        <source>Evgeniy Degtyarev</source>
+        <translation>Евгений Дегтярев</translation>
+    </message>
+</context>
+<context>
+    <name>Maemo5Led</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5led/maemo5led.cpp" line="+39"/>
+        <source>Maemo 5 Led Notifications</source>
+        <translation>Led уведомления за Maemo 5</translation>
+    </message>
+</context>
+<context>
+    <name>Maemo5Settings</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Show led notifications when display On</source>
+        <translation>Да се покзват led уведомленията, когато екрана е включен</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Vibration notifications when display On</source>
+        <translation>Да вибрира, когато екрана е включен</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Vibrate time, ms (50)</source>
+        <translation>Време на вибрирането, мсек. (50)</translation>
+    </message>
+    <message>
+        <location line="+37"/>
+        <source>Popup time, sec (50)</source>
+        <translation>Време на изскачащото уведомление, сек. (50)</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Orientation</source>
+        <translation>Ориентация</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Landscape</source>
+        <translation>Пейзаж </translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Portrait</source>
+        <translation>Портрет</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Auto Orientation</source>
+        <translation>Автоматична</translation>
+    </message>
+</context>
+<context>
+    <name>Maemo5Tray</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5tray/maemo5tray.cpp" line="+38"/>
+        <source>Maemo 5 blink icon in the tray</source>
+        <translation>Мигаща икона в подноса на Maemo 5</translation>
+    </message>
+</context>
+<context>
+    <name>Maemo5Vibra</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5vibra/maemo5vibra.cpp" line="+40"/>
+        <source>Maemo 5 Vibration Notifications</source>
+        <translation>Вибриращи уведомления за Maemo 5</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5led/maemo5ledplugin.cpp" line="-3"/>
+        <source>Maemo Led Notifications</source>
+        <translation>Led уведомления за Maemo</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system based on Maemo 5 Led</source>
+        <translation>Система за уведомления на базата на Maemo 5 Led</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settingsplugin.cpp" line="-3"/>
+        <source>Maemo 5 Settings</source>
+        <translation>Maemo 5 Настройки</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Specific Maemo 5 settings</source>
+        <translation>Специфични за Maemo 5 настройки</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5tray/maemo5trayplugin.cpp" line="+3"/>
+        <location line="+4"/>
+        <source>Maemo 5 Tray</source>
+        <translation>Maemo 5 поднос</translation>
+    </message>
+    <message>
+        <location line="-3"/>
+        <location line="+4"/>
+        <source>Implement Maemo 5 specific system tray</source>
+        <translation>Реализация на специфичния за Maemo 5 системен поднос</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5vibra/maemo5vibraplugin.cpp" line="-3"/>
+        <source>Maemo Vibration Notifications</source>
+        <translation>Вибриращи уведомления за Maemo</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system based on Maemo 5 Vibrator</source>
+        <translation>Система за уведомления на базата на Maemo 5 вибратор</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settingsplugin.cpp" line="+7"/>
+        <source>Maemo 5 Settings</source>
+        <translation>Maemo 5 Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5led/maemo5ledplugin.cpp" line="+3"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settingsplugin.cpp" line="-4"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5tray/maemo5trayplugin.cpp" line="-7"/>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5vibra/maemo5vibraplugin.cpp" line="+3"/>
+        <source>Author</source>
+        <translation>Автор</translation>
+    </message>
+</context>
+<context>
+    <name>plugin</name>
+    <message>
+        <location line="+2"/>
+        <source>Maemo Led Notifications</source>
+        <translation>Led уведомления за Maemo</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system, based on Maemo 5 Led</source>
+        <translation>Система за уведомления на базата на Maemo 5 Led</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5settings/maemo5settingsplugin.cpp" line="+2"/>
+        <source>Maemo 5 Settings</source>
+        <translation>Maemo 5 Настройки</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Specific Maemo 5 settings</source>
+        <translation>Специфични за Maemo 5 настройки</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/maemo5integration/maemo5vibra/maemo5vibraplugin.cpp" line="+2"/>
+        <source>Maemo Vibration Notifications</source>
+        <translation>Вибриращи уведомления за Maemo</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Notification system, based on Maemo 5 Vibrator</source>
+        <translation>Система за уведомления на базата на Maemo 5 вибратор</translation>
+    </message>
+</context>
+</TS>
