88944c4ec98df2ad1ab19e0480d56277f224eb94
MrFree <f1.mrfree@gmail.com>
2011-11-14 22:09:21 +1100

    RU +meegointegration upd

---
+++ ru_RU/meegointegration.ts
@@ -0,0 +1,384 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AboutDialog.qml" line="30"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AboutDialog.qml" line="125"/>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AboutDialog.qml" line="130"/>
+        <source>Translators</source>
+        <translation>Переводчики</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AboutDialog.qml" line="135"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+</context>
+<context>
+    <name>AddContact</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/quickaddcontactdialog.cpp" line="61"/>
+        <source>Add contact</source>
+        <translation>Добавить контакт</translation>
+    </message>
+</context>
+<context>
+    <name>AddContactDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="36"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="70"/>
+        <source>Select protocol:</source>
+        <translation>Выберите протокол:</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="103"/>
+        <source>Add contact for</source>
+        <translation>Добавить контакт для</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="121"/>
+        <source>Contact ID</source>
+        <translation>ID контакта</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="139"/>
+        <source>Name for contact</source>
+        <translation>Имя для контакта</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="150"/>
+        <source>Add contact</source>
+        <translation>Добавить контакт</translation>
+    </message>
+</context>
+<context>
+    <name>AuthDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AuthDialog.qml" line="64"/>
+        <source>Accept</source>
+        <translation>Принять</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/AuthDialog.qml" line="65"/>
+        <source>Reject</source>
+        <translation>Отклонить</translation>
+    </message>
+</context>
+<context>
+    <name>Chat</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/Chat.qml" line="61"/>
+        <source>Write anything</source>
+        <translation type="unfinished">Пишите что хотите</translation>
+    </message>
+</context>
+<context>
+    <name>ContactInfo</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/authdialogwrapper.cpp" line="112"/>
+        <source>Recieved authorizarion request from %1:</source>
+        <translation>Принята авторизация от %1:</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/authdialogwrapper.cpp" line="115"/>
+        <source>Send authorizarion request to %1:</source>
+        <translation type="unfinished">Отправить запрос авторизации %1:</translation>
+    </message>
+</context>
+<context>
+    <name>ContactList</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/ContactList.qml" line="120"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>Core</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/quickaboutdialog.cpp" line="40"/>
+        <source>About qutIM</source>
+        <translation>О qutIM</translation>
+    </message>
+</context>
+<context>
+    <name>JoinGroupChat</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="145"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="114"/>
+        <source>Recent</source>
+        <translation type="unfinished">Последнее</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="146"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="115"/>
+        <source>Bookmarks</source>
+        <translation type="unfinished">Закладки</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="183"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="146"/>
+        <source>Join an existing or create a new groupchat</source>
+        <translation type="unfinished">Присоедениться к существующей или создать новую конференцию</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="185"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="148"/>
+        <source>Join</source>
+        <translation>Войти</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="187"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="150"/>
+        <source>Create, edit, or delete saved bookmarks</source>
+        <translation type="unfinished">Создать, изменить или удалить закладки</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="189"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="152"/>
+        <source>Manage bookmarks</source>
+        <translation type="unfinished">Управление закладками</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/quickjoingroupchat.cpp" line="73"/>
+        <source>Join groupchat</source>
+        <translation>Войти в конференцию</translation>
+    </message>
+</context>
+<context>
+    <name>JoinGroupChatDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="45"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="338"/>
+        <source>Join</source>
+        <translation>Войти</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="46"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="83"/>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="112"/>
+        <source>Select account:</source>
+        <translation type="unfinished">Выбрать аккаунт:</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="223"/>
+        <source>Join to conference:</source>
+        <translation type="unfinished">Войти в конференцию:</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="235"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="253"/>
+        <source>Conference</source>
+        <translation type="unfinished">Конференция</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="271"/>
+        <source>Nick</source>
+        <translation>Ник</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="289"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="308"/>
+        <source>Auto Join</source>
+        <translation type="unfinished">Автовход</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="326"/>
+        <source>Save to bookmark</source>
+        <translation type="unfinished">Сохранить в закладки</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::AboutDialogWrapper</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/aboutdialogwrapper.cpp" line="53"/>
+        <source>Based on Qt %1 (%2 bit).</source>
+        <translation type="unfinished">Основано на библиотеке Qt версии %1 (%2 бит).</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/aboutdialogwrapper.cpp" line="56"/>
+        <source>&lt;div&gt;&lt;b&gt;qutIM&lt;/b&gt; %1 is licensed under GNU General Public License, version 2 or (at your option) any later version.&lt;/div&gt;&lt;div&gt;qutIM resources such as themes, icons, sounds may come along with a different license.&lt;/div&gt;&lt;br&gt;&lt;hr&gt;&lt;br&gt;</source>
+        <translation type="unfinished">&lt;div&gt;&lt;b&gt;qutIM&lt;/b&gt; %1 лицензировано под GNU General Public License, версии 2 или (по вашему желанию) любой более поздней версии.&lt;/div&gt;&lt;div&gt;Ресурсы программы qutIM такие как темы, пиктограммы, звуки могут иметь другие лицензии.&lt;/div&gt;&lt;br&gt;&lt;hr&gt;&lt;br&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::PasswordDialogWrapper</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/passworddialogwrapper.cpp" line="38"/>
+        <source>Enter password for account</source>
+        <translation type="unfinished">Введите пароль аккаунта</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::QuickPasswordDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/quickpassworddialog.cpp" line="35"/>
+        <source>Enter password for account 
+ %1 (%2)</source>
+        <translation type="unfinished">Введите пароль для аккаунта 
+ %1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/PasswordDialog.qml" line="56"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/PasswordDialog.qml" line="65"/>
+        <source>Remember password</source>
+        <translation type="unfinished">Запомнить пароль (это не поможет ВАМ запомнить пароль)</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/PasswordDialog.qml" line="80"/>
+        <source>OK</source>
+        <translation>Ок</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/declarative/meego/PasswordDialog.qml" line="81"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="20"/>
+        <source>Meego application window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="21"/>
+        <source>Main window of application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="25"/>
+        <source>Meego chat page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="26"/>
+        <source>Simple chat based on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="28"/>
+        <source>Meego contact list page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="29"/>
+        <source>Simple contact list on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="31"/>
+        <source>Meego password dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="32"/>
+        <source>Simple password dialog on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="34"/>
+        <source>Meego authorization dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="35"/>
+        <source>Simple authorization dialog on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="37"/>
+        <source>MeeGo Harmattan Idle detector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="38"/>
+        <source>Simple harmattan idle detector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="40"/>
+        <source>MeeGo Add contact dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="41"/>
+        <source>Simple add contact dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="43"/>
+        <source>MeeGo about dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="44"/>
+        <source>Simple about dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="46"/>
+        <source>MeeGo join group chat dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="47"/>
+        <source>Simple join group chat dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="49"/>
+        <source>Accounts Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="50"/>
+        <source>Simple accounts model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="52"/>
+        <source>MeeGo settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="53"/>
+        <source>Simple MeeGo settings dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
