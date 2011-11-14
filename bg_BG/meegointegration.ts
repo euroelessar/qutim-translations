a9a5e39f4d1123b586a0d6599c2ab5cfe8a68062
Boyan G. Kiroff <boyan.kiroff@gmail.com>
2011-11-14 15:48:44 +0200

    BG: Added MeeGo Integration

---
+++ bg_BG/meegointegration.ts
@@ -0,0 +1,384 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/AboutDialog.qml" line="+30"/>
+        <source>Close</source>
+        <translation>Затваряне</translation>
+    </message>
+    <message>
+        <location line="+95"/>
+        <source>Developers</source>
+        <translation>Разработчици</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Translators</source>
+        <translation>Преводачи</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>License</source>
+        <translation>Лиценз</translation>
+    </message>
+</context>
+<context>
+    <name>AddContact</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/quickaddcontactdialog.cpp" line="+61"/>
+        <source>Add contact</source>
+        <translation>Добавяне на контакт</translation>
+    </message>
+</context>
+<context>
+    <name>AddContactDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/AddContactDialog.qml" line="+36"/>
+        <source>Close</source>
+        <translation>Затваряне</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Select protocol:</source>
+        <translation>Избор на протокол:</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Add contact for</source>
+        <translation>Добавяне на контакт на</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Contact ID</source>
+        <translation>ID на контакта</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Name for contact</source>
+        <translation>Име на контакта</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Add contact</source>
+        <translation>Добавяне на контакта</translation>
+    </message>
+</context>
+<context>
+    <name>AuthDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/AuthDialog.qml" line="+64"/>
+        <source>Accept</source>
+        <translation>Приемане</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Reject</source>
+        <translation>Отхвърляне</translation>
+    </message>
+</context>
+<context>
+    <name>Chat</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/Chat.qml" line="+61"/>
+        <source>Write anything</source>
+        <translation>Напишете нещо</translation>
+    </message>
+</context>
+<context>
+    <name>ContactInfo</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/authdialogwrapper.cpp" line="+112"/>
+        <source>Recieved authorizarion request from %1:</source>
+        <translation>Получена е заявка за удостоверение от %1:</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Send authorizarion request to %1:</source>
+        <translation>Изпращане на заявка за удостоверение от %1:</translation>
+    </message>
+</context>
+<context>
+    <name>ContactList</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/ContactList.qml" line="+120"/>
+        <source>Cancel</source>
+        <translation>Отказ</translation>
+    </message>
+</context>
+<context>
+    <name>Core</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/quickaboutdialog.cpp" line="+40"/>
+        <source>About qutIM</source>
+        <translation>За qutIM</translation>
+    </message>
+</context>
+<context>
+    <name>JoinGroupChat</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/bookmarksmodel.cpp" line="+145"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+114"/>
+        <source>Recent</source>
+        <translation>Последни</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+1"/>
+        <source>Bookmarks</source>
+        <translation>Отметки</translation>
+    </message>
+    <message>
+        <location line="+37"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+31"/>
+        <source>Join an existing or create a new groupchat</source>
+        <translation>Присъединяване към съществуващ или създаване на нов групов разговор</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+2"/>
+        <source>Join</source>
+        <translation>Присъединяване</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+2"/>
+        <source>Create, edit, or delete saved bookmarks</source>
+        <translation>Създаване, редактиране или изтриване на отметки</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/joingroupchatwrapper.cpp" line="+2"/>
+        <source>Manage bookmarks</source>
+        <translation>Управление на отметки</translation>
+    </message>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/quickjoingroupchat.cpp" line="+73"/>
+        <source>Join groupchat</source>
+        <translation>Присъединяване към групов разговор</translation>
+    </message>
+</context>
+<context>
+    <name>JoinGroupChatDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/JoinGroupChatDialog.qml" line="+45"/>
+        <location line="+293"/>
+        <source>Join</source>
+        <translation>Присъединяване</translation>
+    </message>
+    <message>
+        <location line="-292"/>
+        <source>Close</source>
+        <translation>Затваряне</translation>
+    </message>
+    <message>
+        <location line="+37"/>
+        <location line="+29"/>
+        <source>Select account:</source>
+        <translation>Избор на сметка:</translation>
+    </message>
+    <message>
+        <location line="+111"/>
+        <source>Join to conference:</source>
+        <translation>Присъединяване към конференция:</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Conference</source>
+        <translation>Конференция</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Nick</source>
+        <translation>Псевдоним</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Password</source>
+        <translation>Парола</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Auto Join</source>
+        <translation>Автоматично присъединяване</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Save to bookmark</source>
+        <translation>Запис в отметките</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::AboutDialogWrapper</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/aboutdialogwrapper.cpp" line="+53"/>
+        <source>Based on Qt %1 (%2 bit).</source>
+        <translation>Базиран на Qt %1 (%2 bit).</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>&lt;div&gt;&lt;b&gt;qutIM&lt;/b&gt; %1 is licensed under GNU General Public License, version 2 or (at your option) any later version.&lt;/div&gt;&lt;div&gt;qutIM resources such as themes, icons, sounds may come along with a different license.&lt;/div&gt;&lt;br&gt;&lt;hr&gt;&lt;br&gt;</source>
+        <translation>&lt;div&gt;&lt;b&gt;qutIM&lt;/b&gt; %1 е лицензиран под GNU General Public License, версия 2 или (по ваше усмотрение) всяка последваща версия.&lt;/div&gt;&lt;div&gt;Ресурсите, които ползва qutIM напр. теми, икони, звуци могат да бъдат под друг лиценз.&lt;/div&gt;&lt;br&gt;&lt;hr&gt;&lt;br&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::PasswordDialogWrapper</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/passworddialogwrapper.cpp" line="+38"/>
+        <source>Enter password for account</source>
+        <translation>Въведете парола за сметката</translation>
+    </message>
+</context>
+<context>
+    <name>MeegoIntegration::QuickPasswordDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/quickpassworddialog.cpp" line="+35"/>
+        <source>Enter password for account 
+ %1 (%2)</source>
+        <translation>Въведете парола за сметката 
+ %1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/declarative/meego/PasswordDialog.qml" line="+56"/>
+        <source>Password</source>
+        <translation>Парола</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Remember password</source>
+        <translation>Запомняне на паролата</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Cancel</source>
+        <translation>Отказ</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../qutim/plugins/meegointegration/meegoapplicationwindow/src/applicationwindowplugin.cpp" line="+20"/>
+        <source>Meego application window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Main window of application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Meego chat page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple chat based on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Meego contact list page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple contact list on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Meego password dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple password dialog on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Meego authorization dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple authorization dialog on QML elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>MeeGo Harmattan Idle detector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple harmattan idle detector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>MeeGo Add contact dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple add contact dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>MeeGo about dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple about dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>MeeGo join group chat dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple join group chat dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Accounts Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple accounts model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>MeeGo settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Simple MeeGo settings dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
